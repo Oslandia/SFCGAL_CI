ff7639550d480a6486026aa69b5e2886d743aa30
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2022-01-12 14:36:18 +0100

    modernize

--- src/algorithm/alphaShapes.cpp
+++ src/algorithm/alphaShapes.cpp
@@ -1,15 +1,5 @@
-/**
- * Part of the My Project, under the MIT license.
- * See LICENSE.txt for license information.
- *
- * SPDX-License-Identifier: MIT
- */
-
-/**
- * \file        : alphaShapes
- * \author      : Loïc Bartoletti (loic dot bartoletti @ oslandia dot com)
- * \created     : novembre 2021
- */
+// Copyright (c) 2022-2022, Oslandia.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <SFCGAL/algorithm/alphaShapes.h>
 
@@ -39,35 +29,35 @@
 namespace SFCGAL {
 namespace algorithm {
 
-typedef CGAL::Alpha_shape_vertex_base_2<Kernel>      Vb;
-typedef CGAL::Alpha_shape_face_base_2<Kernel>        Fb;
-typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<Kernel, Tds>  Triangulation_2;
-typedef CGAL::Point_2<Kernel>                        Point_2;
-typedef CGAL::Segment_2<Kernel>                      Segment_2;
-typedef CGAL::Alpha_shape_2<Triangulation_2>         Alpha_shape_2;
+using Vb              = CGAL::Alpha_shape_vertex_base_2<Kernel>;
+using Fb              = CGAL::Alpha_shape_face_base_2<Kernel>;
+using Tds             = CGAL::Triangulation_data_structure_2<Vb, Fb>;
+using Triangulation_2 = CGAL::Delaunay_triangulation_2<Kernel, Tds>;
+using Point_2         = CGAL::Point_2<Kernel>;
+using Segment_2       = CGAL::Segment_2<Kernel>;
+using Alpha_shape_2   = CGAL::Alpha_shape_2<Triangulation_2>;
 
-typedef Alpha_shape_2::Alpha_shape_edges_iterator Alpha_shape_edges_iterator;
+using Alpha_shape_edges_iterator = Alpha_shape_2::Alpha_shape_edges_iterator;
 
-typedef CGAL::Arr_non_caching_segment_basic_traits_2<Kernel> Traits_2;
-// typedef CGAL::Arr_segment_traits_2<Kernel> Traits_2;
-typedef CGAL::Arrangement_2<Traits_2> Arrangement;
+using Traits_2    = CGAL::Arr_non_caching_segment_basic_traits_2<Kernel>;
+using Arrangement = CGAL::Arrangement_2<Traits_2>;
 
 template <class OutputIterator>
 void
 alpha_edges(const Alpha_shape_2 &A, OutputIterator out)
 {
-  Alpha_shape_edges_iterator it  = A.alpha_shape_edges_begin(),
-                             end = A.alpha_shape_edges_end();
+  auto it  = A.alpha_shape_edges_begin();
+  auto end = A.alpha_shape_edges_end();
   for (; it != end; ++it) {
-    if (A.classify(*it) == 2)
+    if (A.classify(*it) == 2) {
       *out++ = A.segment(*it);
+    }
   }
 }
 
-static double
+static auto
 computeAlpha(const Geometry &g, Alpha_shape_2 &alphaShape, double alpha = 0,
-             size_t nb_components = 1)
+             size_t nb_components = 1) -> double
 {
   using CGAL::object_cast;
   double result = -1.0;
@@ -80,15 +70,14 @@ computeAlpha(const Geometry &g, Alpha_shape_2 &alphaShape, double alpha = 0,
   const_cast<Geometry &>(g).accept(getPointVisitor);
 
   // collect points
-
   if (getPointVisitor.points.size() < 4) {
     return result;
   }
 
   std::vector<Point_2> points;
 
-  for (size_t i = 0; i < getPointVisitor.points.size(); i++) {
-    points.push_back(getPointVisitor.points[i]->toPoint_2());
+  for (auto &point : getPointVisitor.points) {
+    points.push_back(point->toPoint_2());
   }
 
   std::vector<Segment_2> segments;
@@ -96,19 +85,14 @@ computeAlpha(const Geometry &g, Alpha_shape_2 &alphaShape, double alpha = 0,
   alphaShape.set_alpha(Kernel::FT(alpha));
   alpha_edges(alphaShape, std::back_inserter(segments));
 
-  /* if (segments.size() == 0) { */
-  /*   std::cout << "0 segment"; */
-  /*   return result; */
-  /* } */
-
   result = CGAL::to_double(*alphaShape.find_optimal_alpha(nb_components));
 
-  std::cout << result << "\n";
   return result;
 }
 
-static std::unique_ptr<Geometry>
+static auto
 alpha_to_geometry(const Alpha_shape_2 &A, bool allow_holes)
+    -> std::unique_ptr<Geometry>
 {
   std::vector<Segment_2> segments;
   alpha_edges(A, std::back_inserter(segments));
@@ -116,9 +100,9 @@ alpha_to_geometry(const Alpha_shape_2 &A, bool allow_holes)
   Arrangement arr;
 
   CGAL::insert_non_intersecting_curves(arr, segments.begin(), segments.end());
-  Polygon *poly = new Polygon;
+  auto *poly = new Polygon;
   for (auto f = arr.faces_begin(); f != arr.faces_end(); f++) {
-    LineString *ring = new LineString;
+    auto *ring = new LineString;
     for (auto h = f->holes_begin(); h != f->holes_end(); h++) {
       auto he = *h;
       do {
@@ -139,8 +123,9 @@ alpha_to_geometry(const Alpha_shape_2 &A, bool allow_holes)
   return std::unique_ptr<Geometry>(poly);
 }
 
-std::unique_ptr<Geometry>
+auto
 optimal_alpha_shapes(const Geometry &g, bool allow_holes, size_t nb_components)
+    -> std::unique_ptr<Geometry>
 {
   Alpha_shape_2 A;
   const double  optimalAlpha = computeAlpha(g, A, 10000, nb_components);
@@ -153,8 +138,9 @@ optimal_alpha_shapes(const Geometry &g, bool allow_holes, size_t nb_components)
   return alpha_to_geometry(A, allow_holes);
 }
 
-std::unique_ptr<Geometry>
+auto
 alphaShapes(const Geometry &g, double alpha, bool allow_holes)
+    -> std::unique_ptr<Geometry>
 {
   using CGAL::object_cast;
   Alpha_shape_2 A;
