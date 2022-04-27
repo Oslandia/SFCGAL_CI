6739ea5d43c91da9a8445492152fb94e93b4e10c
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2022-04-27 17:07:43 +0200

    [CI] Add OpenSuse test

---
+++ ci/opensuse/before_install.sh
@@ -0,0 +1,8 @@
+sudo zypper update -y
+sudo zypper install -y \
+    cmake cgal-devel ninja gcc gcc-g++ \
+    libboost_serialization-devel \
+    libboost_timer-devel \
+    libboost_program_options-devel \
+    libboost_filesystem-devel \
+    libboost_test-devel
