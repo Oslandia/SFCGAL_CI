7177f5a7f732461950a4e54a2bb627d9303d3c80
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2023-10-31 08:27:21 +0100

    RH-like: replace centos8 by fedora38

--- ci/fedora/before_install.sh
+++ ci/fedora/before_install.sh
@@ -1,6 +1,7 @@
 sudo yum update -qy
 sudo yum install -y \
-    cmake boost boost-devel gmp gmp-c++ gmp-devel mpfr mpfr-devel make
+    cmake boost boost-devel gmp gmp-c++ gmp-devel mpfr mpfr-devel make \
+    xz
 
 #CGAL
 wget https://github.com/CGAL/cgal/releases/download/v"$1"/CGAL-"$1".tar.xz
