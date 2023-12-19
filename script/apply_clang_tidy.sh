e0d4ad4573ed4bf312f42e9ed6a9174905c353cc
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2023-12-19 11:08:25 +0100

    apply_clang_tidy: add comments/usage

--- script/apply_clang_tidy.sh
+++ script/apply_clang_tidy.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# This script generates a commit for each clang-tidy check using various clang utilities.
+#
+# By default, it uses Linux paths.
+# You can execute it by defining the variables CLANG_TIDY_BIN, RUN_CLANG_TIDY_BIN, 
+# and APPLY_CLANG_TIDY_BIN when running the script:
+#
+# > env CLANG_TIDY_BIN=/usr/local/bin/clang-tidy15 \
+#       RUN_CLANG_TIDY_BIN=/usr/local/bin/run-clang-tidy15 \
+#       APPLY_CLANG_TIDY_BIN=/usr/local/bin/clang-apply-replacements15 \
+#       ./apply_clang_tidy.sh
+#
+# There isn't a clang-tidy CI in this project as of now.
+# This script is useful for potential changes in .clang-tidy.
+# We encourage developers to apply the fixes suggested by clang-tidy.
+
+
 CLANG_TIDY_BIN=${CLANG_TIDY_BIN:-/usr/bin/clang-tidy}
 RUN_CLANG_TIDY_BIN=${RUN_CLANG_TIDY_BIN:-/usr/bin/run-clang-tidy}
 APPLY_CLANG_TIDY_BIN=${APPLY_CLANG_TIDY_BIN:-/usr/bin/clang-apply-replacements}
