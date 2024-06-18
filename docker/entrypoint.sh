6a921238d0735acf763b44c64fdd442ea047365b
Jean Felder <jean.felder@oslandia.com>
2024-06-18 16:34:33 +0200

    ci: Build a debian docker image and push it to the registry
    This change allows to build a debian stable docker image of `sfcgal`
    and publish it to the registry.
    The docker image is built on 3 different occasions:
    - a new Merge Request
    - a new tag
    - a push to master
    
    These images will be used by `pysfcgal` and `qsfcgal` to avoid
    rebuilding sfcgal in their own repositories.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/SFCGAL/build/src:${LD_LIBRARY_PATH}
+export LIBRARY_PATH=/SFCGAL/build/src:${LIBRARY_PATH}
+export CPLUS_INCLUDE_PATH=/SFCGAL/build/include:${CPLUS_INCLUDE_PATH}
+export C_INCLUDE_PATH=/SFCGAL/build/include:${CPLUS_INCLUDE_PATH}
+
+exec "$@"
