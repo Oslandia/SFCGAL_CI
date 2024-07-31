91170f574e68c9df6b995fb304372a8ba2b9a92d
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2024-07-31 08:00:05 +0200

    Add a Sphere (3D) Class

---
+++ src/Sphere.h
@@ -0,0 +1,190 @@
+// Copyright (c) 2024-2024, Oslandia.
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#ifndef _SFCGAL_SPHERE_H_
+#define _SFCGAL_SPHERE_H_
+
+#include <optional>
+#include <vector>
+
+#include <CGAL/Polyhedron_3.h>
+
+#include "SFCGAL/Kernel.h"
+#include "SFCGAL/export.h"
+
+namespace SFCGAL {
+
+/**
+ * @brief Represents a sphere in 3D space
+ *
+ * This class provides methods to generate a polyhedron and a point cloud
+ * representation of a sphere. It uses SFCGAL's Kernel for exact computations.
+ */
+class SFCGAL_API Sphere {
+public:
+  /**
+   * @brief Constructs a Sphere object
+   * @param radius The radius of the sphere
+   * @param center The center point of the sphere
+   * @param num_vertical The number of vertical divisions
+   * @param num_horizontal The number of horizontal divisions
+   */
+  Sphere(const Kernel::FT      &radius = 1.0,
+         const Kernel::Point_3 &center = Kernel::Point_3(0, 0, 0),
+         int num_vertical = 16, int num_horizontal = 32);
+
+  /**
+   * @brief Copy constructor
+   */
+  Sphere(const Sphere &other) = default;
+
+  /**
+   * @brief Assignment operator
+   */
+  Sphere &
+  operator=(Sphere other);
+
+  /**
+   * @brief Destructor
+   */
+  ~Sphere() = default;
+
+  /**
+   * @brief Sets the radius of the sphere
+   * @param radius The new radius
+   */
+  inline void
+  setRadius(const Kernel::FT &radius)
+  {
+    m_radius = radius;
+    invalidateCache();
+  }
+
+  /**
+   * @brief Sets the center of the sphere
+   * @param center The new center point
+   */
+  inline void
+  setCenter(const Kernel::Point_3 &center)
+  {
+    m_center = center;
+    invalidateCache();
+  }
+
+  /**
+   * @brief Sets the number of vertical divisions
+   * @param num The new number of vertical divisions
+   */
+  inline void
+  setNumVertical(int num)
+  {
+    m_num_vertical = num;
+    invalidateCache();
+  }
+
+  /**
+   * @brief Sets the number of horizontal divisions
+   * @param num The new number of horizontal divisions
+   */
+  inline void
+  setNumHorizontal(int num)
+  {
+    m_num_horizontal = num;
+    invalidateCache();
+  }
+
+  /**
+   * @brief Gets the radius of the sphere
+   * @return The radius
+   */
+  inline const Kernel::FT &
+  radius() const
+  {
+    return m_radius;
+  }
+
+  /**
+   * @brief Gets the center of the sphere
+   * @return The center point
+   */
+  inline const Kernel::Point_3 &
+  center() const
+  {
+    return m_center;
+  }
+
+  /**
+   * @brief Gets the number of vertical divisions
+   * @return The number of vertical divisions
+   */
+  inline int
+  numVertical() const
+  {
+    return m_num_vertical;
+  }
+
+  /**
+   * @brief Gets the number of horizontal divisions
+   * @return The number of horizontal divisions
+   */
+  inline int
+  numHorizontal() const
+  {
+    return m_num_horizontal;
+  }
+
+  /**
+   * @brief Generates a polyhedron representation of the sphere
+   * @return A CGAL::Polyhedron_3 object representing the sphere
+   */
+  CGAL::Polyhedron_3<Kernel>
+  generatePolyhedron();
+
+  /**
+   * @brief Generates a point cloud representation of the sphere
+   * @return A vector of Point_3 objects representing points on the sphere's
+   * surface
+   */
+  std::vector<Kernel::Point_3>
+  generatePoints();
+
+  /**
+   * @brief Volume of the Sphere
+   * @return Returns the volume of the sphere
+   */
+  double
+  volume() const
+  {
+    return CGAL::to_double((4.0 / 3.0) * m_radius * m_radius * m_radius *
+                           CGAL_PI);
+  }
+
+  /**
+   * @brief Area of the Sphere
+   * @return Returns the area of the sphere
+   */
+  double
+  area() const
+  {
+    return CGAL::to_double(4 * m_radius * m_radius * CGAL_PI);
+  }
+
+private:
+  Kernel::FT                                  m_radius;
+  Kernel::Point_3                             m_center;
+  int                                         m_num_vertical;
+  int                                         m_num_horizontal;
+  std::optional<CGAL::Polyhedron_3<Kernel>>   m_polyhedron;
+  std::optional<std::vector<Kernel::Point_3>> m_points;
+
+  void
+  invalidateCache();
+  CGAL::Polyhedron_3<Kernel>
+  generateSpherePolyhedron();
+  std::vector<Kernel::Point_3>
+  generateSpherePoints();
+};
+
+} // namespace SFCGAL
+
+#endif // _SFCGAL_SPHERE_H_
---
+++ test/unit/SFCGAL/SphereTest.cpp
@@ -0,0 +1,98 @@
+#include "SFCGAL/Sphere.h"
+#include "SFCGAL/PolyhedralSurface.h"
+#include "SFCGAL/io/wkt.h"
+#include <boost/test/unit_test.hpp>
+#include <cmath>
+
+using namespace SFCGAL;
+using Point_3 = Kernel::Point_3;
+
+BOOST_AUTO_TEST_SUITE(SphereTests)
+
+BOOST_AUTO_TEST_CASE(testDefaultConstructor)
+{
+    Sphere sphere;
+    BOOST_CHECK_CLOSE(sphere.radius(), 1.0, 1e-6);
+    BOOST_CHECK_EQUAL(sphere.center(), Point_3(0, 0, 0));
+    BOOST_CHECK_EQUAL(sphere.numVertical(), 16);
+    BOOST_CHECK_EQUAL(sphere.numHorizontal(), 32);
+}
+
+BOOST_AUTO_TEST_CASE(testCustomConstructor)
+{
+    Point_3 center(1, 2, 3);
+    Sphere sphere(2.0, center, 20, 40);
+    BOOST_CHECK_CLOSE(sphere.radius(), 2.0, 1e-6);
+    BOOST_CHECK_EQUAL(sphere.center(), center);
+    BOOST_CHECK_EQUAL(sphere.numVertical(), 20);
+    BOOST_CHECK_EQUAL(sphere.numHorizontal(), 40);
+}
+
+BOOST_AUTO_TEST_CASE(testSetters)
+{
+    Sphere sphere;
+    sphere.setRadius(3.0);
+    sphere.setCenter(Point_3(1, 1, 1));
+    sphere.setNumVertical(24);
+    sphere.setNumHorizontal(48);
+
+    BOOST_CHECK_CLOSE(sphere.radius(), 3.0, 1e-6);
+    BOOST_CHECK_EQUAL(sphere.center(), Point_3(1, 1, 1));
+    BOOST_CHECK_EQUAL(sphere.numVertical(), 24);
+    BOOST_CHECK_EQUAL(sphere.numHorizontal(), 48);
+}
+
+BOOST_AUTO_TEST_CASE(testGeneratePolyhedron)
+{
+    Sphere sphere(1.0, Point_3(0, 0, 0), 8, 16);
+    auto polyhedron = sphere.generatePolyhedron();
+
+    // The number of vertices should be (numVertical - 1) * numHorizontal + 2
+    BOOST_CHECK_EQUAL(polyhedron.size_of_vertices(), 7 * 16 + 2);
+
+    // The number of faces should be (numVertical -1) * numHorizontal * 2
+    BOOST_CHECK_EQUAL(polyhedron.size_of_facets(), 7 * 16 * 2);
+}
+
+BOOST_AUTO_TEST_CASE(testGeneratePoints)
+{
+    Sphere sphere(1.0, Point_3(0, 0, 0), 8, 16);
+    auto points = sphere.generatePoints();
+
+    // The number of points should be numVertical * numHorizontal
+    BOOST_CHECK_EQUAL(points.size(), 8 * 16);
+
+    // Check that all points are at a distance of 1 from the center
+    for (const auto& point : points) {
+        BOOST_CHECK_CLOSE(CGAL::sqrt(CGAL::to_double(CGAL::squared_distance(Point_3(0,0,0), point))), 1.0, 1e-6);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(testVolume)
+{
+    Sphere sphere(2.0, Point_3(0, 0, 0), 32, 64);
+    double volume = sphere.volume();
+    double expected_volume = 4.0 / 3.0 * M_PI * 2.0 * 2.0 * 2.0;
+    BOOST_CHECK_CLOSE(volume, expected_volume, 0.1); // 0.1% tolerance
+}
+
+BOOST_AUTO_TEST_CASE(testSurfaceArea)
+{
+    Sphere sphere(2.0, Point_3(0, 0, 0), 32, 64);
+    double area = sphere.area();
+    double expected_area = 4.0 * M_PI * 2.0 * 2.0;
+    BOOST_CHECK_CLOSE(area, expected_area, 0.1); // 0.1% tolerance
+}
+
+BOOST_AUTO_TEST_CASE(testWKT)
+{
+    Sphere sphere(1.0, Point_3(0, 0, 0), 4, 8);
+    auto polyhedron = sphere.generatePolyhedron();
+    PolyhedralSurface surface(polyhedron);
+    std::string wkt = surface.asText(1);
+    
+    BOOST_TEST_MESSAGE("Sphere WKT: " << wkt);
+    BOOST_CHECK(wkt.find("POLYHEDRALSURFACE Z") == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
