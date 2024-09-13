4f88a939b83b1062ab5e963e184cca1998c12fab
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2024-09-13 17:26:11 +0200

    fix(clang18): Fix build for clang18, missing includes

--- test/unit/SFCGAL/algorithm/UnionTest.cpp
+++ test/unit/SFCGAL/algorithm/UnionTest.cpp
@@ -18,12 +18,16 @@
  *   License along with this library; if not, see
  <http://www.gnu.org/licenses/>.
  */
+#include "SFCGAL/algorithm/union.h"
 #include "SFCGAL/Exception.h"
+#include "SFCGAL/Geometry.h"
+#include "SFCGAL/GeometryCollection.h"
+#include "SFCGAL/Polygon.h"
+#include "SFCGAL/Solid.h"
 #include "SFCGAL/algorithm/area.h"
 #include "SFCGAL/algorithm/isValid.h"
 #include "SFCGAL/algorithm/tesselate.h"
 #include "SFCGAL/algorithm/translate.h"
-#include "SFCGAL/algorithm/union.h"
 #include "SFCGAL/algorithm/volume.h"
 #include "SFCGAL/detail/tools/Registry.h"
 #include "SFCGAL/triangulate/triangulatePolygon.h"
