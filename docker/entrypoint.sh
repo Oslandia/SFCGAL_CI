c08561be4706df05d6a0c2d1efaad8516f6def3e
Jean Felder <jean.felder@oslandia.com>
2024-09-24 19:05:12 +0200

    docker: Fix C_INCLUDE_PATH variable in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@
 export LD_LIBRARY_PATH=/SFCGAL/build/src:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=/SFCGAL/build/src:${LIBRARY_PATH}
 export CPLUS_INCLUDE_PATH=/SFCGAL/build/include:${CPLUS_INCLUDE_PATH}
-export C_INCLUDE_PATH=/SFCGAL/build/include:${CPLUS_INCLUDE_PATH}
+export C_INCLUDE_PATH=/SFCGAL/build/include:${C_INCLUDE_PATH}
 
 exec "$@"
