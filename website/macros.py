c5940fed68f27d10446635e0c5a740fe0f5de001
Julien Moura <julien.moura@oslandia.com>
2024-11-15 17:11:16 +0100

    fix(quality): apply git hooks to existing documentation

--- website/macros.py
+++ website/macros.py
@@ -1,5 +1,3 @@
-import os
-
 def define_env(env):
     """Define variables, macros, and filters.
 
@@ -9,15 +7,16 @@ def define_env(env):
     Returns:
         None
     """
+
     @env.macro
-    def include_file(filename: str) ->str:
+    def include_file(filename: str) -> str:
         """Include the contents of a file.
 
         Args:
             filename (str): The name of the file to include.
 
         Returns:
-            str: The content of the file with specific replacements made, 
+            str: The content of the file with specific replacements made,
                  or an error message if the file is not found.
 
         Raises:
@@ -26,17 +25,19 @@ def define_env(env):
         try:
             with open(filename, "r", encoding="utf-8") as file:
                 content = file.read().replace("website/docs/", "")
-                content = content.replace("[AUTHORS](AUTHORS)", "[AUTHORS](./authors.md)")
+                content = content.replace(
+                    "[AUTHORS](AUTHORS)", "[AUTHORS](./authors.md)"
+                )
                 return content
         except FileNotFoundError:
             return f"File not found: {filename}"
-        
+
     @env.macro
     def get_project_version() -> str:
         """Retrieve the project version from sonar-project.properties.
 
         Returns:
-            str: The project version if found, or an error message if the file 
+            str: The project version if found, or an error message if the file
                  cannot be read or the version is not specified.
         """
         properties_file = "../sonar-project.properties"
