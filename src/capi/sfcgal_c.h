Oslandia/SFCGAL_CI

commit a2cce61227d7a66265bfc72fb658fe92786712ea

feat(apic): add sfcgal_geometry_is_equals function

src/capi/sfcgal_c.h
@@ -1261,6 +1261,32 @@ SFCGAL_API sfcgal_geometry_t *
 sfcgal_geometry_centroid_3d(const sfcgal_geometry_t *geom);
 
 /**
+ * Returns true if geom1 is equals to geom2.
+ *
+ * For each point of geom1 there is a point in geom2.
+ * @pre isValid(geom) == true
+ * @post isValid(return) == true
+ * @ingroup capi
+ */
+SFCGAL_API int
+sfcgal_geometry_is_equals(const sfcgal_geometry_t *geom1,
+                          const sfcgal_geometry_t *geom2);
+
+/**
+ * Returns true if geom1 is almost equals to geom2.
+ *
+ * For each point of geom1 there is a point in geom2 within tolerance distance.
+ * @param tolerance 0 by default
+ * @pre isValid(geom) == true
+ * @post isValid(return) == true
+ * @ingroup capi
+ */
+SFCGAL_API int
+sfcgal_geometry_is_almost_equals(const sfcgal_geometry_t *geom1,
+                                 const sfcgal_geometry_t *geom2,
+                                 double                   tolerance = 0.0);
+
+/**
  * Returns the y monotone partition of a geometry (polygon without hole)
  * @pre isValid(geom) == true
  * @post isValid(return) == true

src/capi/sfcgal_c.cpp
@@ -1987,3 +1987,32 @@ sfcgal_geometry_centroid_3d(const sfcgal_geometry_t *geom)
   std::unique_ptr<SFCGAL::Geometry> out(result->clone());
   return out.release();
 }
+
+extern "C" auto
+sfcgal_geometry_is_equals(const sfcgal_geometry_t *ga,
+                          const sfcgal_geometry_t *gb) -> int
+{
+  return sfcgal_geometry_is_almost_equals(ga, gb, 0.0);
+}
+
+extern "C" auto
+sfcgal_geometry_is_almost_equals(const sfcgal_geometry_t *ga,
+                                 const sfcgal_geometry_t *gb, double tolerance)
+    -> int
+{
+  const auto *g1 = reinterpret_cast<const SFCGAL::Geometry *>(ga);
+  const auto *g2 = reinterpret_cast<const SFCGAL::Geometry *>(gb);
+
+  bool result;
+  try {
+    result = g1->almostEqual(*g2, tolerance);
+  } catch (std::exception &e) {
+    SFCGAL_WARNING("During is_almost_equals(A, B, %g):", tolerance);
+    SFCGAL_WARNING("  with A: %s", g1->asText().c_str());
+    SFCGAL_WARNING("  with B: %s", g2->asText().c_str());
+    SFCGAL_ERROR("%s", e.what());
+    result = false;
+  }
+
+  return static_cast<int>(result);
+}

test/unit/SFCGAL/capi/sfcgal_cTest.cpp
@@ -162,6 +162,42 @@ BOOST_AUTO_TEST_CASE(testIsSimple)
   sfcgal_free_buffer(reason);
 }
 
+BOOST_AUTO_TEST_CASE(testIsEqual)
+{
+  sfcgal_set_error_handlers(printf, on_error);
+
+  {
+    // same wkt
+    std::unique_ptr<Geometry> const g(io::readWkt("LINESTRING (0.0 0.0, 2.0 0.0, 1.0 1.0)"));
+    std::unique_ptr<Geometry> const g2(io::readWkt("LINESTRING (0.0 0.0, 2.0 0.0, 1.0 1.0)"));
+    // check
+    hasError = false;
+    BOOST_CHECK_EQUAL(true, sfcgal_geometry_is_equals(g.get(), g2.get()));
+    BOOST_CHECK(hasError == false);
+  }
+
+  {
+    // 0.1 diff between wkt
+    std::unique_ptr<Geometry> const g(io::readWkt("LINESTRING (0.0 0.0, 2.0 0.0, 1.0 1.0)"));
+    std::unique_ptr<Geometry> const g2(io::readWkt("LINESTRING (0.1 0.1, 2.1 0.1, 1.1 1.1)"));
+    // check
+    hasError = false;
+    BOOST_CHECK_EQUAL( true, sfcgal_geometry_is_almost_equals( g.get(), g2.get(), 0.11 ) );
+    BOOST_CHECK(hasError == false);
+  }
+
+  {
+    // retrieve wkb from geometry via C++ api
+    std::unique_ptr<Geometry> const g(io::readWkt("LINESTRING (0.0 0.0, 2.0 0.0, 1.0 1.0)"));
+    std::unique_ptr<Geometry> const g2(io::readWkt("LINESTRING (0.1 0.1, 2.1 0.1, 1.1 1.1)"));
+    // check
+    hasError = false;
+    BOOST_CHECK_EQUAL( true, sfcgal_geometry_is_almost_equals( g.get(), g2.get(), 0.100008 ) );
+    BOOST_CHECK_EQUAL( false, sfcgal_geometry_is_almost_equals( g.get(), g2.get(), 0.099993 ) );
+    BOOST_CHECK(hasError == false);
+  }
+}
+
 /// Coordinate() ;
 BOOST_AUTO_TEST_CASE(testErrorOnBadGeometryType)
 {

commit 0f0f42c2ed0390069d20f89a7218fa62d7f86c68

fix(capi): Remove default value for sfcgal_geometry_is_almost_equals
This breaks the API because the C language does not handle default
values.

Closes: https://gitlab.com/sfcgal/SFCGAL/-/issues/300
Fixes: a2cce61227d7a66265bfc72fb658fe92786712ea

src/capi/sfcgal_c.h
@@ -1276,7 +1276,7 @@ sfcgal_geometry_is_equals(const sfcgal_geometry_t *geom1,
  * Returns true if geom1 is almost equals to geom2.
  *
  * For each point of geom1 there is a point in geom2 within tolerance distance.
- * @param tolerance 0 by default
+ * @param tolerance
  * @pre isValid(geom) == true
  * @post isValid(return) == true
  * @ingroup capi
@@ -1284,7 +1284,7 @@ sfcgal_geometry_is_equals(const sfcgal_geometry_t *geom1,
 SFCGAL_API int
 sfcgal_geometry_is_almost_equals(const sfcgal_geometry_t *geom1,
                                  const sfcgal_geometry_t *geom2,
-                                 double                   tolerance = 0.0);
+                                 double                   tolerance);
 
 /**
  * Returns the y monotone partition of a geometry (polygon without hole)