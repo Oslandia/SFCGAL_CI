11c77412767bc84f5e46ee20fa78e5d27c0b7129
Jean Felder <jean.felder@oslandia.com>
2025-03-23 09:16:50 +0100

    feat(test/regress): Add a test to check that the c api works
    At the moment, there is no real c test to ensure that the C API always
    works. From time to time, there can be some regressions (see for
    example, https://gitlab.com/sfcgal/SFCGAL/-/merge_requests/435).
    
    This adds a simple c test to unit/regress directory. If this program
    does not compile, this probably means that C API is broken.

---
+++ test/regress/capi/main.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (C) 2025 Oslandia
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "SFCGAL/capi/sfcgal_c.h"
+
+
+/* This is a very simple c program to check that the C API compiles.
+ */
+int main(void)
+{
+  char wkt_str[] = "LINESTRING (0.0 0.0,2.0 0.0,1.0 1.0)";
+  sfcgal_geometry_t *geometry = sfcgal_io_read_wkt( wkt_str, strlen(wkt_str) );
+
+  char *wkt_buffer;
+  size_t wkt_len;
+  sfcgal_geometry_as_text_decim(geometry, 1, &wkt_buffer, &wkt_len);
+
+  if (strcmp(wkt_buffer, wkt_str))
+  {
+    fprintf(stderr, "WKT do not match\n");
+    sfcgal_geometry_delete(geometry);
+    sfcgal_free_buffer(wkt_buffer);
+    return EXIT_FAILURE;
+  }
+
+  printf("WKT match\n");
+  sfcgal_geometry_delete(geometry);
+  sfcgal_free_buffer(wkt_buffer);
+
+  return EXIT_SUCCESS;
+}
