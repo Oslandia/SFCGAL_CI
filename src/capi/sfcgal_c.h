Oslandia/SFCGAL_CI

commit de89601144a5c82c02314757e77663236e3a4f04

fix(capi): deprecate function sfcgal_geometry_is_complexity_detail and add sfcgal_geometry_is_simple_detail
Rename sfcgal_geometry_is_complexity_detail to keep logic with sfcgal_geometry_is_simple.

src/capi/sfcgal_c.h
@@ -103,6 +103,16 @@ sfcgal_geometry_is_valid_detail(const sfcgal_geometry_t *geom,
                                 sfcgal_geometry_t      **invalidity_location);
 
 /**
+ * @deprecated Same as sfcgal_geometry_is_valid_detail
+ * @ingroup capi
+ */
+[[__deprecated__(
+    "Use sfcgal_geometry_is_simple_detail instead.")]] SFCGAL_API int
+sfcgal_geometry_is_complexity_detail(const sfcgal_geometry_t *geom,
+                                     char                   **invalidity_reason,
+                                     sfcgal_geometry_t **invalidity_location);
+
+/**
  * Tests if the given geometry is simple or not
  * @ingroup capi
  */
@@ -119,8 +129,8 @@ sfcgal_geometry_is_simple(const sfcgal_geometry_t *);
  * @ingroup capi
  */
 SFCGAL_API int
-sfcgal_geometry_is_complexity_detail(const sfcgal_geometry_t *geom,
-                                     char **complexity_reason);
+sfcgal_geometry_is_simple_detail(const sfcgal_geometry_t *geom,
+                                 char                   **complexity_reason);
 
 /**
  * Tests if the given geometry is 3D or not

src/capi/sfcgal_c.cpp
@@ -198,6 +198,17 @@ sfcgal_geometry_is_valid(const sfcgal_geometry_t *geom) -> int
           *reinterpret_cast<const SFCGAL::Geometry *>(geom)));)
 }
 
+// deprecated!
+extern "C" auto
+sfcgal_geometry_is_complexity_detail(const sfcgal_geometry_t *geom,
+                                     char                   **invalidity_reason,
+                                     sfcgal_geometry_t **invalidity_location)
+    -> int
+{
+  return sfcgal_geometry_is_valid_detail(geom, invalidity_reason,
+                                         invalidity_location);
+}
+
 extern "C" auto
 sfcgal_geometry_is_valid_detail(const sfcgal_geometry_t *geom,
                                 char                   **invalidity_reason,

commit 028b622ac90967ae506d0ed1e5bcf2c689325890

fix(capi): Fix deprecated usage
The `deprecated` or `__deprecated__` keywords are not handled by all
the versions of the compilers. For example, the current version of
clang in debian stable does not handle it.

In order to fix this issue, provide a fallback mechanism by
introducing a "SFCGAL_DEPRECATED" macro which should work in all
cases.

See for example: https://gitlab.com/sfcgal/SFCGAL/-/jobs/9501302274
Fixes: de89601144a5c82c02314757e77663236e3a4f04

src/capi/sfcgal_c.h
@@ -14,6 +14,35 @@
 extern "C" {
 #endif
 
+#if defined(__has_c_attribute)
+  #if __has_c_attribute(deprecated)
+    #define SFCGAL_DEPRECATED(msg) [[deprecated(msg)]]
+  #elif defined(__clang__) || defined(__GNUC__)
+    #define SFCGAL_DEPRECATED(msg) __attribute__((deprecated(msg)))
+  #elif defined(_MSC_VER)
+    #define SFCGAL_DEPRECATED(msg) __declspec(deprecated(msg))
+  #else
+    #define SFCGAL_DEPRECATED(msg)
+  #endif
+#elif defined(__has_cpp_attribute)
+  #if __has_cpp_attribute(deprecated)
+    #define SFCGAL_DEPRECATED(msg) [[deprecated(msg)]]
+  #elif defined(__clang__) || defined(__GNUC__)
+    #define SFCGAL_DEPRECATED(msg) __attribute__((deprecated(msg)))
+  #elif defined(_MSC_VER)
+    #define SFCGAL_DEPRECATED(msg) __declspec(deprecated(msg))
+  #else
+    #define SFCGAL_DEPRECATED(msg)
+  #endif
+#elif defined(__clang__) || defined(__GNUC__)
+  #define SFCGAL_DEPRECATED(msg) __attribute__((deprecated(msg)))
+#elif defined(_MSC_VER)
+  #define SFCGAL_DEPRECATED(msg) __declspec(deprecated(msg))
+#else
+  #pragma message("WARNING: deprecated if not supported by this compiler")
+  #define SFCGAL_DEPRECATED(msg)
+#endif
+
 // TODO : return of errors ! => error handler
 
 /*--------------------------------------------------------------------------------------*
@@ -106,8 +135,8 @@ sfcgal_geometry_is_valid_detail(const sfcgal_geometry_t *geom,
  * @deprecated Use sfcgal_geometry_is_valid_detail instead.
  * @ingroup capi
  */
-[[__deprecated__(
-    "Use sfcgal_geometry_is_simple_detail instead.")]] SFCGAL_API int
+SFCGAL_DEPRECATED("Use sfcgal_geometry_is_valid_detail instead")
+SFCGAL_API int
 sfcgal_geometry_is_complexity_detail(const sfcgal_geometry_t *geom,
                                      char                   **invalidity_reason,
                                      sfcgal_geometry_t **invalidity_location);