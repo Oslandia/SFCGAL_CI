Oslandia/SFCGAL_CI

commit 1b91a5a39611b129ab364b5b426c25d8daafbe7d

fix(triangulatedsurface): Use correct plural form for patch
It's `Patches`, not `Patchs`.

src/TriangulatedSurface.h
@@ -91,8 +91,8 @@ public:
    * [SFA/OGC]Returns the number of patchs
    * @deprecated see numGeometries()
    */
-  inline size_t
-  numPatchs() const
+  inline auto
+  numPatches() const -> size_t
   {
     return _triangles.size();
   }

src/PolyhedralSurface.cpp
@@ -29,7 +29,7 @@ PolyhedralSurface::PolyhedralSurface(const std::unique_ptr<Geometry> &geometry)
   } else if (geometry->is<TriangulatedSurface>()) {
     const TriangulatedSurface &triangulatedSurface =
         geometry->as<TriangulatedSurface>();
-    for (size_t i = 0; i < triangulatedSurface.numPatchs(); ++i) {
+    for (size_t i = 0; i < triangulatedSurface.numPatches(); ++i) {
       this->addPatch(triangulatedSurface.patchN(i));
     }
   } else if (geometry->is<Polygon>()) {

src/Transform.cpp
@@ -110,7 +110,7 @@ Transform::visit(PolyhedralSurface &g)
 void
 Transform::visit(TriangulatedSurface &g)
 {
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     visit(g.patchN(i));
   }
 }

src/TriangulatedSurface.cpp
@@ -139,7 +139,7 @@ TriangulatedSurface::addTriangles(const TriangulatedSurface &other)
 auto
 TriangulatedSurface::patchN(size_t const &n) const -> const Triangle &
 {
-  if (n >= numPatchs()) {
+  if (n >= numPatches()) {
     BOOST_THROW_EXCEPTION(Exception(
         (boost::format("Cannot access geometry at position %s. "
                        "TriangulatedSurface has only %d geometries.") %
@@ -153,11 +153,11 @@ TriangulatedSurface::patchN(size_t const &n) const -> const Triangle &
 auto
 TriangulatedSurface::patchN(size_t const &n) -> Triangle &
 {
-  if (n >= numPatchs()) {
+  if (n >= numPatches()) {
     BOOST_THROW_EXCEPTION(Exception(
         (boost::format("Cannot access geometry at position %s. "
                        "TriangulatedSurface has only %d geometries.") %
-         n % numPatchs())
+         n % numPatches())
             .str()));
   }
 
@@ -169,11 +169,11 @@ TriangulatedSurface::setPatchN(Triangle *triangle, size_t const &n)
 {
   BOOST_ASSERT(triangle != NULL);
 
-  if (n >= numPatchs()) {
+  if (n >= numPatches()) {
     BOOST_THROW_EXCEPTION(Exception(
         (boost::format("Cannot set geometry at position %s. "
                        "TriangulatedSurface has only %d geometries.") %
-         n % numPatchs())
+         n % numPatches())
             .str()));
   }
 
@@ -241,7 +241,7 @@ public:
   void
   operator()(HDS &hds) override
   {
-    const size_t nrPatchs = surf.numPatchs();
+    const size_t nrPatchs = surf.numPatches();
     // Postcondition: `hds' is a valid polyhedral surface.
     CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
     B.begin_surface(/* vertices */ nrPatchs * 3,

src/algorithm/ConsistentOrientationBuilder.cpp
@@ -23,7 +23,7 @@ void
 ConsistentOrientationBuilder::addTriangulatedSurface(
     const TriangulatedSurface &triangulatedSurface)
 {
-  for (size_t i = 0; i < triangulatedSurface.numPatchs(); i++) {
+  for (size_t i = 0; i < triangulatedSurface.numPatches(); i++) {
     addTriangle(triangulatedSurface.patchN(i));
   }
 }

src/algorithm/area.cpp
@@ -129,7 +129,7 @@ area(const TriangulatedSurface &g) -> double
 {
   double result = 0.0;
 
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     result += area(g.patchN(i));
   }
 
@@ -285,7 +285,7 @@ area3D(const TriangulatedSurface &g) -> double
 {
   double result = 0.0;
 
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     result += area3D(g.patchN(i));
   }
 

src/algorithm/connection.cpp
@@ -125,7 +125,7 @@ SurfaceGraph::SurfaceGraph(const PolyhedralSurface &surf)
 SurfaceGraph::SurfaceGraph(const TriangulatedSurface &tin)
     : _numVertices(0), _isValid(Validity::valid())
 {
-  const size_t numPatchs = tin.numPatchs();
+  const size_t numPatchs = tin.numPatches();
 
   for (size_t t = 0; t != numPatchs; ++t) { // for each polygon
     const FaceIndex idx = boost::add_vertex(_graph);

src/algorithm/distance3d.cpp
@@ -221,7 +221,7 @@ distancePointTriangulatedSurface3D(
 
   double dMin = std::numeric_limits<double>::infinity();
 
-  for (size_t i = 0; i < triangulatedSurfaceB.numPatchs(); i++) {
+  for (size_t i = 0; i < triangulatedSurfaceB.numPatches(); i++) {
     dMin = std::min(dMin, distancePolygonGeometry3D(
                               triangulatedSurfaceB.patchN(i), pointA));
   }
@@ -388,7 +388,7 @@ distanceLineStringTriangulatedSurface3D(
 
   double dMin = std::numeric_limits<double>::infinity();
 
-  for (size_t i = 0; i < triangulatedSurfaceB.numPatchs(); i++) {
+  for (size_t i = 0; i < triangulatedSurfaceB.numPatches(); i++) {
     dMin = std::min(
         dMin, distancePolygonGeometry3D(triangulatedSurfaceB.patchN(i), lineA));
   }
@@ -498,7 +498,7 @@ distanceTriangleTriangulatedSurface3D(
 
   double dMin = std::numeric_limits<double>::infinity();
 
-  for (size_t i = 0; i < triangulatedSurfaceB.numPatchs(); i++) {
+  for (size_t i = 0; i < triangulatedSurfaceB.numPatches(); i++) {
     dMin = std::min(dMin, triangulatedSurfaceB.patchN(i).distance3D(triangleA));
   }
 
@@ -578,7 +578,7 @@ distanceTriangulatedSurfaceGeometry3D(
 
   double dMin = std::numeric_limits<double>::infinity();
 
-  for (size_t i = 0; i < triangulatedSurfaceA.numPatchs(); i++) {
+  for (size_t i = 0; i < triangulatedSurfaceA.numPatches(); i++) {
     dMin = std::min(dMin, triangulatedSurfaceA.patchN(i).distance3D(geomB));
   }
 

commit d0ea9b5841ff10378e408ea154b9930cc51d2826

fix(triangulatedsurface): Fix patches typo in documentation
Fixes: 1b91a5a39611b129ab364b5b426c25d8daafbe7d

src/TriangulatedSurface.h
@@ -88,7 +88,7 @@ public:
   swapXY() -> void override;
 
   /**
-   * [SFA/OGC]Returns the number of patchs
+   * [SFA/OGC]Returns the number of patches
    * @deprecated see numGeometries()
    */
   inline auto