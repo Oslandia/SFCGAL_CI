Oslandia/SFCGAL_CI

commit e04d1aae8fc2262b0c1be9aa46afdde87673b230

fix(polyhedralsurface): Use correct plural form for patch
It's `Patches`, not `Patchs`.

src/capi/sfcgal_c.h
@@ -665,22 +665,22 @@ sfcgal_polyhedral_surface_create();
  * @ingroup capi
  */
 SFCGAL_API size_t
-sfcgal_polyhedral_surface_num_patchs(const sfcgal_geometry_t *polyhedral);
+sfcgal_polyhedral_surface_num_patches(const sfcgal_geometry_t *polyhedral);
 
 /**
  * Returns the number of polygons of a given PolyhedralSurface
  * @pre polyhedral must be a PolyhedralSurface
- * @deprecated Use sfcgal_polyhedral_surface_num_patchs instead.
+ * @deprecated Use sfcgal_polyhedral_surface_num_patches instead.
  * @ingroup capi
  */
-SFCGAL_DEPRECATED("Use sfcgal_polyhedral_surface_num_patchs instead.")
+SFCGAL_DEPRECATED("Use sfcgal_polyhedral_surface_num_patches instead.")
 SFCGAL_API size_t
 sfcgal_polyhedral_surface_num_polygons(const sfcgal_geometry_t *polyhedral);
 
 /**
  * Returns the ith patch of a given PolyhedralSurface
  * @pre polyhedral must be a PolyhedralSurface
- * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patchs(polyhedral)
+ * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patches(polyhedral)
  * @post the returned Polygon is not writable and must not be deallocated by the
  * caller
  * @ingroup capi
@@ -692,7 +692,7 @@ sfcgal_polyhedral_surface_patch_n(const sfcgal_geometry_t *polyhedral,
 /**
  * Returns the ith polygon of a given PolyhedralSurface
  * @pre polyhedral must be a PolyhedralSurface
- * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patchs(polyhedral)
+ * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patches(polyhedral)
  * @post the returned Polygon is not writable and must not be deallocated by the
  * caller
  * @deprecated Use sfcgal_polyhedral_surface_patch_n instead
@@ -719,7 +719,7 @@ sfcgal_polyhedral_surface_add_patch(sfcgal_geometry_t *polyhedral,
  * Set the ith patch of a given PolyhedralSurface
  * @pre polyhedral must be a PolyhedralSurface.
  * @pre patch must be a Polygon.
- * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patchs(polyhedral)
+ * @pre i >= 0 and i < sfcgal_polyhedral_surface_num_patches(polyhedral)
  * The ownership of the polygon is taken. The caller is not responsible anymore
  * of its deallocation.
  * @ingroup capi

src/GeometryVisitor.cpp
@@ -73,7 +73,7 @@ GeometryVisitor::visit(Geometry &g)
 /////
 // void GeometryVisitor::visit( PolyhedralSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//	for ( size_t i = 0; i < g.numPatches(); i++ ){
 //		visit( g.patchN(i) );
 //	}
 //}
@@ -83,7 +83,7 @@ GeometryVisitor::visit(Geometry &g)
 /////
 // void GeometryVisitor::visit( TriangulatedSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//	for ( size_t i = 0; i < g.numPatches(); i++ ){
 //		visit( g.patchN(i) );
 //	}
 //}
@@ -144,7 +144,7 @@ ConstGeometryVisitor::visit(const Geometry &g)
 /////
 // void ConstGeometryVisitor::visit( const PolyhedralSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//	for ( size_t i = 0; i < g.numPatches(); i++ ){
 //		visit( g.patchN(i) );
 //	}
 //}
@@ -154,7 +154,7 @@ ConstGeometryVisitor::visit(const Geometry &g)
 /////
 // void ConstGeometryVisitor::visit( const TriangulatedSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//	for ( size_t i = 0; i < g.numPatches(); i++ ){
 //		visit( g.patchN(i) );
 //	}
 //}

src/PolyhedralSurface.cpp
@@ -203,7 +203,7 @@ PolyhedralSurface::addPatch(Polygon *patch)
 void
 PolyhedralSurface::addPatchs(const PolyhedralSurface &polyhedralSurface)
 {
-  for (size_t i = 0; i < polyhedralSurface.numPatchs(); i++) {
+  for (size_t i = 0; i < polyhedralSurface.numPatches(); i++) {
     addPatch(polyhedralSurface.patchN(i));
   }
 }
@@ -231,11 +231,11 @@ PolyhedralSurface::setPatchN(Polygon *patch, size_t const &n)
 {
   BOOST_ASSERT(polygon != NULL);
 
-  if (n >= numPatchs()) {
+  if (n >= numPatches()) {
     BOOST_THROW_EXCEPTION(
         Exception((boost::format("Cannot set geometry at position %s. "
                                  "PolyhedralSurface has only %d geometries.") %
-                   n % numPatchs())
+                   n % numPatches())
                       .str()));
   }
 

src/PolyhedralSurface.h
@@ -139,8 +139,8 @@ public:
    * @warning PolyhedralSurface is treated as one geometry. numGeometries
    * returns 1 or 0 for empty PolyhedralSurface
    */
-  inline size_t
-  numPatchs() const
+  inline auto
+  numPatches() const -> size_t
   {
     return _polygons.size();
   }
@@ -150,13 +150,13 @@ public:
    *
    * @warning PolyhedralSurface is treated as one geometry. numGeometries
    * returns 1 or 0 for empty PolyhedralSurface
-   * @deprecated see numPatchs
+   * @deprecated see numPatches
    * @see numGeometries()
    */
   inline size_t
   numPolygons() const
   {
-    return numPatchs();
+    return numPatches();
   }
 
   /**

src/Transform.cpp
@@ -102,7 +102,7 @@ Transform::visit(GeometryCollection &g)
 void
 Transform::visit(PolyhedralSurface &g)
 {
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     visit(g.patchN(i));
   }
 }

src/TriangulatedSurface.h
@@ -130,7 +130,7 @@ public:
 
   /**
    * [SFA/OGC]Returns the number of points
-   * @deprecated see numPatchs()
+   * @deprecated see numPatches()
    */
   inline size_t
   numTriangles() const

src/algorithm/area.cpp
@@ -141,7 +141,7 @@ area(const PolyhedralSurface &g) -> double
 {
   double result = 0.0;
 
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     result += area(g.patchN(i));
   }
 
@@ -273,7 +273,7 @@ area3D(const PolyhedralSurface &g) -> double
 {
   double area = 0.0;
 
-  for (size_t i = 0; i < g.numPatchs(); i++) {
+  for (size_t i = 0; i < g.numPatches(); i++) {
     area += area3D(g.patchN(i));
   }
 

src/algorithm/connection.cpp
@@ -107,9 +107,9 @@ SurfaceGraph::addRing(const LineString &ring, FaceIndex faceIndex)
 SurfaceGraph::SurfaceGraph(const PolyhedralSurface &surf)
     : _numVertices(0), _isValid(Validity::valid())
 {
-  const size_t numPatchs = surf.numPatchs();
+  const size_t numPatches = surf.numPatches();
 
-  for (size_t p = 0; p != numPatchs; ++p) { // for each polygon
+  for (size_t p = 0; p != numPatches; ++p) { // for each polygon
     const FaceIndex idx = boost::add_vertex(_graph);
     BOOST_ASSERT(idx == p);
     (void)idx;
@@ -125,9 +125,9 @@ SurfaceGraph::SurfaceGraph(const PolyhedralSurface &surf)
 SurfaceGraph::SurfaceGraph(const TriangulatedSurface &tin)
     : _numVertices(0), _isValid(Validity::valid())
 {
-  const size_t numPatchs = tin.numPatches();
+  const size_t numPatches = tin.numPatches();
 
-  for (size_t t = 0; t != numPatchs; ++t) { // for each polygon
+  for (size_t t = 0; t != numPatches; ++t) { // for each polygon
     const FaceIndex idx = boost::add_vertex(_graph);
     BOOST_ASSERT(idx == t);
     (void)idx;

commit e143b67671942f2ddb54cab4d1fbc06a0b844332

fix(capi): Fix patches typo in documentation
fixes: e04d1aae8fc2262b0c1be9aa46afdde87673b230 and
1b91a5a39611b129ab364b5b426c25d8daafbe7d

src/capi/sfcgal_c.h
@@ -660,7 +660,7 @@ SFCGAL_API sfcgal_geometry_t *
 sfcgal_polyhedral_surface_create();
 
 /**
- * Returns the number of patchs of a given PolyhedralSurface
+ * Returns the number of patches of a given PolyhedralSurface
  * @pre polyhedral must be a PolyhedralSurface
  * @ingroup capi
  */
@@ -750,7 +750,7 @@ SFCGAL_API sfcgal_geometry_t *
 sfcgal_triangulated_surface_create();
 
 /**
- * Returns the number of patchs of a given TriangulatedSurface
+ * Returns the number of patches of a given TriangulatedSurface
  * @pre tin must be a TriangulatedSurface
  * @ingroup capi
  */