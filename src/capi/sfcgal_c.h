Oslandia/SFCGAL_CI

commit b4e92835a4e7f38b8cd32472fc85c41a0fa395f9

refactor(triangulatedsurface): Add a patch logic
According to the OGC standard, triangles are supposed to be called
`Patch`. Use this terminology and deprecate the triangle methods.

src/capi/sfcgal_c.h
@@ -762,27 +762,58 @@ SFCGAL_API sfcgal_geometry_t *
 sfcgal_triangulated_surface_create();
 
 /**
- * Returns the number of triangles of a given TriangulatedSurface
+ * Returns the number of patchs of a given TriangulatedSurface
  * @pre tin must be a TriangulatedSurface
- * @deprecated Use sfcgal_geometry_num_geometries instead
  * @ingroup capi
  */
-SFCGAL_DEPRECATED("Use sfcgal_geometry_num_geometries instead.")
+SFCGAL_API size_t
+sfcgal_triangulated_surface_num_patchs(const sfcgal_geometry_t *tin);
+
+/**
+ * Returns the number of triangles of a given TriangulatedSurface
+ * @pre tin must be a TriangulatedSurface
+ * @deprecated Use sfcgal_triangulated_surface_num_patchs instead.
+ * @ingroup capi
+ */
+SFCGAL_DEPRECATED("Use sfcgal_triangulated_surface_num_patchs instead.")
 SFCGAL_API size_t
 sfcgal_triangulated_surface_num_triangles(const sfcgal_geometry_t *tin);
 
 /**
- * Returns the ith Triangle of a given TriangulatedSurface
+ * Returns the ith Patch of a given TriangulatedSurface
  * @pre tin must be a TriangulatedSurface
- * @pre i >= 0 and i < sfcgal_triangulated_surface_num_triangles( tin )
+ * @pre i >= 0 and i < sfcgal_triangulated_surface_num_patchs( tin )
  * @post the returned Triangle is not writable and must not be deallocated by
  * the caller
- * @deprecated Use sfcgal_geometry_get_geometry_n
  * @ingroup capi
  */
-SFCGAL_DEPRECATED("Use sfcgal_geometry_get_geometry_n instead.")
+SFCGAL_API const sfcgal_geometry_t *
+sfcgal_triangulated_surface_patch_n(const sfcgal_geometry_t *tin, size_t i);
+
+/**
+ * Returns the ith Triangle of a given TriangulatedSurface
+ * @pre tin must be a TriangulatedSurface
+ * @pre i >= 0 and i < sfcgal_triangulated_surface_num_patchs( tin )
+ * @post the returned Triangle is not writable and must not be deallocated by
+ * the caller
+ * @deprecated Use sfcgal_triangulated_surface_patch_n instead.
+ * @ingroup capi
+ */
+SFCGAL_DEPRECATED("Use sfcgal_triangulated_surface_patch_n instead.")
 SFCGAL_API const sfcgal_geometry_t *
 sfcgal_triangulated_surface_triangle_n(const sfcgal_geometry_t *tin, size_t i);
+
+/**
+ * Adds a Patch to a given TriangulatedSurface
+ * @pre tin must be a TriangulatedSurface
+ * @pre patch must be a Triangle
+ * @post the ownership of the Triangle is taken. The caller is not responsible
+ * anymore of its deallocation
+ * @ingroup capi
+ */
+SFCGAL_API void
+sfcgal_triangulated_surface_add_patch(sfcgal_geometry_t *tin,
+                                      sfcgal_geometry_t *patch);
 
 /**
  * Adds a Triangle to a given TriangulatedSurface

src/GeometryVisitor.cpp
@@ -83,8 +83,8 @@ GeometryVisitor::visit(Geometry &g)
 /////
 // void GeometryVisitor::visit( TriangulatedSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numTriangles(); i++ ){
-//		visit( g.triangleN(i) );
+//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//		visit( g.patchN(i) );
 //	}
 //}
 
@@ -154,8 +154,8 @@ ConstGeometryVisitor::visit(const Geometry &g)
 /////
 // void ConstGeometryVisitor::visit( const TriangulatedSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numTriangles(); i++ ){
-//		visit( g.triangleN(i) );
+//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//		visit( g.patchN(i) );
 //	}
 //}
 //

src/PolyhedralSurface.cpp
@@ -29,8 +29,8 @@ PolyhedralSurface::PolyhedralSurface(const std::unique_ptr<Geometry> &geometry)
   } else if (geometry->is<TriangulatedSurface>()) {
     const TriangulatedSurface &triangulatedSurface =
         geometry->as<TriangulatedSurface>();
-    for (size_t i = 0; i < triangulatedSurface.numTriangles(); ++i) {
-      this->addPatch(triangulatedSurface.triangleN(i));
+    for (size_t i = 0; i < triangulatedSurface.numPatchs(); ++i) {
+      this->addPatch(triangulatedSurface.patchN(i));
     }
   } else if (geometry->is<Polygon>()) {
     this->addPatch(geometry->as<Polygon>());

src/TriangulatedSurface.cpp
@@ -123,11 +123,17 @@ TriangulatedSurface::swapXY() -> void
 }
 
 void
-TriangulatedSurface::addTriangles(const TriangulatedSurface &other)
+TriangulatedSurface::addPatchs(const TriangulatedSurface &other)
 {
   for (const auto &it : other) {
-    addTriangle(it);
+    addPatch(it);
   }
+}
+
+void
+TriangulatedSurface::addTriangles(const TriangulatedSurface &other)
+{
+  return addPatchs(other);
 }
 
 auto

src/TriangulatedSurface.h
@@ -88,8 +88,57 @@ public:
   swapXY() -> void override;
 
   /**
-   * [SFA/OGC]Returns the number of points
+   * [SFA/OGC]Returns the number of patchs
    * @deprecated see numGeometries()
+   */
+  inline size_t
+  numPatchs() const
+  {
+    return _triangles.size();
+  }
+  /**
+   * [SFA/OGC]Returns the n-th patch
+   */
+  inline const Triangle &
+  patchN(size_t const &n) const
+  {
+    BOOST_ASSERT(n < _triangles.size());
+    return _triangles[n];
+  }
+  /**
+   * [SFA/OGC]Returns the n-th patch
+   */
+  inline Triangle &
+  patchN(size_t const &n)
+  {
+    BOOST_ASSERT(n < _triangles.size());
+    return _triangles[n];
+  }
+  /**
+   * add a Patch to the TriangulatedSurface
+   */
+  inline void
+  addPatch(const Triangle &patch)
+  {
+    addPatch(patch.clone());
+  }
+  /**
+   * add a Patch to the TriangulatedSurface
+   */
+  inline void
+  addPatch(Triangle *patch)
+  {
+    _triangles.push_back(patch);
+  }
+  /**
+   * add patchs from an other TriangulatedSurface
+   */
+  void
+  addPatchs(const TriangulatedSurface &other);
+
+  /**
+   * [SFA/OGC]Returns the number of points
+   * @deprecated see numPatchs()
    */
   inline size_t
   numTriangles() const
@@ -98,7 +147,7 @@ public:
   }
   /**
    * [SFA/OGC]Returns the n-th point
-   * @deprecated see geometryN()
+   * @deprecated see patchN()
    */
   inline const Triangle &
   triangleN(size_t const &n) const
@@ -108,7 +157,7 @@ public:
   }
   /**
    * [SFA/OGC]Returns the n-th point
-   * @deprecated see geometryN()
+   * @deprecated see patchN()
    */
   inline Triangle &
   triangleN(size_t const &n)
@@ -118,6 +167,7 @@ public:
   }
   /**
    * add a Triangle to the TriangulatedSurface
+   * @deprecated see addPatch()
    */
   inline void
   addTriangle(const Triangle &triangle)
@@ -126,6 +176,7 @@ public:
   }
   /**
    * add a Triangle to the TriangulatedSurface
+   * @deprecated see addPatch()
    */
   inline void
   addTriangle(Triangle *triangle)
@@ -134,6 +185,7 @@ public:
   }
   /**
    * add triangles from an other TriangulatedSurface
+   * @deprecated see addPatchs()
    */
   void
   addTriangles(const TriangulatedSurface &other);

src/algorithm/ConsistentOrientationBuilder.cpp
@@ -35,7 +35,7 @@ ConsistentOrientationBuilder::buildTriangulatedSurface() -> TriangulatedSurface
   TriangulatedSurface triangulatedSurface;
 
   for (size_t i = 0; i < numTriangles(); i++) {
-    triangulatedSurface.addTriangle(triangleN(i));
+    triangulatedSurface.addPatch(triangleN(i));
   }
 
   return triangulatedSurface;

src/algorithm/area.cpp
@@ -129,8 +129,8 @@ area(const TriangulatedSurface &g) -> double
 {
   double result = 0.0;
 
-  for (size_t i = 0; i < g.numTriangles(); i++) {
-    result += area(g.triangleN(i));
+  for (size_t i = 0; i < g.numPatchs(); i++) {
+    result += area(g.PatchN(i));
   }
 
   return result;

src/algorithm/connection.cpp
@@ -125,13 +125,13 @@ SurfaceGraph::SurfaceGraph(const PolyhedralSurface &surf)
 SurfaceGraph::SurfaceGraph(const TriangulatedSurface &tin)
     : _numVertices(0), _isValid(Validity::valid())
 {
-  const size_t numTriangles = tin.numTriangles();
+  const size_t numPatchs = tin.numPatchs();
 
-  for (size_t t = 0; t != numTriangles; ++t) { // for each polygon
+  for (size_t t = 0; t != numPatchs; ++t) { // for each polygon
     const FaceIndex idx = boost::add_vertex(_graph);
     BOOST_ASSERT(idx == t);
     (void)idx;
-    const Triangle &triangle = tin.triangleN(t);
+    const Triangle &triangle = tin.patchN(t);
     addRing(triangle.toPolygon().exteriorRing(), t);
   }
 }

commit fc1f23fa427027b9ca87d29a5f6944de651eed83

fix(capi): Mark sfcgal_triangulated_surface_add_triangle as deprecated
sfcgal_triangulated_surface_add_patch should be used instead.

Fixes: b4e92835a4e7f38b8cd32472fc85c41a0fa395f9

src/capi/sfcgal_c.h
@@ -823,8 +823,10 @@ sfcgal_triangulated_surface_add_patch(sfcgal_geometry_t *tin,
  * @pre triangle must be a Triangle
  * @post the ownership of the Triangle is taken. The caller is not responsible
  * anymore of its deallocation
+ * @deprecated Use sfcgal_triangulated_surface_add_patch instead.
  * @ingroup capi
  */
+SFCGAL_DEPRECATED("Use sfcgal_triangulated_surface_add_patch instead.")
 SFCGAL_API void
 sfcgal_triangulated_surface_add_triangle(sfcgal_geometry_t *tin,
                                          sfcgal_geometry_t *triangle);