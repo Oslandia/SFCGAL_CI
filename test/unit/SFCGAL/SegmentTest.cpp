88652506d39d1afede7e3de00e7496d1a993a1c8
Jean Felder <jean.felder@oslandia.com>
2025-06-05 17:02:49 +0200

    chore(test): Use SPDX-License-Identifier everywhere

--- test/unit/SFCGAL/SegmentTest.cpp
+++ test/unit/SFCGAL/SegmentTest.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2012-2013, IGN France.
+// Copyright (c) 2012-2024, Oslandia.
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
 // Copyright (c) 2025-2025, SFCGAL Team.
 // SPDX-License-Identifier: LGPL-2.0-or-later
 #include <SFCGAL/Exception.h>
