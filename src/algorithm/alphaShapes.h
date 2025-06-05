272ebdbb4aa438e3af5106ff9dcc7e19a1103dda
Jean Felder <jean.felder@oslandia.com>
2025-06-05 16:47:47 +0200

    chore(src): Use SPDX-License-Identifier everywhere

--- src/algorithm/alphaShapes.h
+++ src/algorithm/alphaShapes.h
@@ -1,22 +1,5 @@
-/**
- *   SFCGAL
- *
- *   Copyright (C) 2021 Loïc Bartoletti - Oslandia <infos@oslandia.com>
- *
- *   This library is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU Library General Public
- *   License as published by the Free Software Foundation; either
- *   version 2 of the License, or (at your option) any later version.
- *
- *   This library is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *   Library General Public License for more details.
-
- *   You should have received a copy of the GNU Library General Public
- *   License along with this library; if not, see
- <http://www.gnu.org/licenses/>.
- */
+// Copyright (c) 2022-2024, Oslandia.
+// SPDX-License-Identifier: LGPL-2.0-or-later
 
 #ifndef SFCGAL_ALGORITHM_ALPHASHAPES_H_
 #define SFCGAL_ALGORITHM_ALPHASHAPES_H_
