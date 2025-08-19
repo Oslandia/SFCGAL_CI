Oslandia/SFCGAL_CI

commit edb8addff0bedb343344dfa636c74d7c6c263912

refactor(polyhedralsurface): Add a patch logic
According to the OGC standard, polygons are supposed to be called
`Patch`. Use this terminology and deprecate the polygon methods.

src/PolyhedralSurface.cpp
@@ -30,10 +30,10 @@ PolyhedralSurface::PolyhedralSurface(const std::unique_ptr<Geometry> &geometry)
     const TriangulatedSurface &triangulatedSurface =
         geometry->as<TriangulatedSurface>();
     for (size_t i = 0; i < triangulatedSurface.numTriangles(); ++i) {
-      this->addPolygon(triangulatedSurface.triangleN(i));
+      this->addPatch(triangulatedSurface.triangleN(i));
     }
   } else if (geometry->is<Polygon>()) {
-    this->addPolygon(geometry->as<Polygon>());
+    this->addPatch(geometry->as<Polygon>());
   } else {
     throw std::invalid_argument("Cannot convert geometry to PolyhedralSurface");
   }
@@ -188,24 +188,42 @@ PolyhedralSurface::toTriangulatedSurface() const -> TriangulatedSurface
 }
 
 void
+PolyhedralSurface::addPatch(const Polygon &patch)
+{
+  addPatch(patch.clone());
+}
+
+void
+PolyhedralSurface::addPatch(Polygon *patch)
+{
+  BOOST_ASSERT(polygon != NULL);
+  _polygons.push_back(patch);
+}
+
+void
+PolyhedralSurface::addPatchs(const PolyhedralSurface &polyhedralSurface)
+{
+  for (size_t i = 0; i < polyhedralSurface.numPatchs(); i++) {
+    addPatch(polyhedralSurface.patchN(i));
+  }
+}
+
+void
 PolyhedralSurface::addPolygon(const Polygon &polygon)
 {
-  addPolygon(polygon.clone());
+  return addPatch(polygon);
 }
 
 void
 PolyhedralSurface::addPolygon(Polygon *polygon)
 {
-  BOOST_ASSERT(polygon != NULL);
-  _polygons.push_back(polygon);
+  return addPatch(polygon);
 }
 
 void
 PolyhedralSurface::addPolygons(const PolyhedralSurface &polyhedralSurface)
 {
-  for (size_t i = 0; i < polyhedralSurface.numPolygons(); i++) {
-    addPolygon(polyhedralSurface.polygonN(i));
-  }
+  return addPatchs(polyhedralSurface);
 }
 
 auto

src/Envelope.cpp
@@ -164,7 +164,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(b);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   // top : e,f,g,h
@@ -176,7 +176,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(h);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   // front : a,b,f,e
@@ -188,7 +188,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(e);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   // back : c,d,h,g
@@ -200,7 +200,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(g);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   // right : b,c,g,f
@@ -212,7 +212,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(f);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   // left : a,e,h,d
@@ -224,7 +224,7 @@ Envelope::toShell() const -> std::unique_ptr<PolyhedralSurface>
     ring.addPoint(d);
     ring.addPoint(ring.startPoint());
 
-    shell->addPolygon(Polygon(ring));
+    shell->addPatch(Polygon(ring));
   }
 
   return shell;

src/GeometryVisitor.cpp
@@ -73,8 +73,8 @@ GeometryVisitor::visit(Geometry &g)
 /////
 // void GeometryVisitor::visit( PolyhedralSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPolygons(); i++ ){
-//		visit( g.polygonN(i) );
+//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//		visit( g.patchN(i) );
 //	}
 //}
 //
@@ -144,8 +144,8 @@ ConstGeometryVisitor::visit(const Geometry &g)
 /////
 // void ConstGeometryVisitor::visit( const PolyhedralSurface & g )
 //{
-//	for ( size_t i = 0; i < g.numPolygons(); i++ ){
-//		visit( g.polygonN(i) );
+//	for ( size_t i = 0; i < g.numPatchs(); i++ ){
+//		visit( g.patchN(i) );
 //	}
 //}
 //

src/PolyhedralSurface.h
@@ -135,46 +135,91 @@ public:
   toTriangulatedSurface() const;
 
   /**
+   * [SFA/OGC]Returns the number of patchs
+   */
+  inline size_t
+  numPatchs() const
+  {
+    return _polygons.size();
+  }
+
+  /**
    * [SFA/OGC]Returns the number of polygons
-   * @deprecated see numGeometries
+   * @deprecated see numPatchs
    */
   inline size_t
   numPolygons() const
   {
-    return _polygons.size();
+    return numPatchs();
+  }
+
+  /**
+   * [SFA/OGC]Returns the n-th patch
+   */
+  inline const Polygon &
+  patchN(size_t const &n) const
+  {
+    BOOST_ASSERT(n < _polygons.size());
+    return _polygons[n];
   }
   /**
+   * [SFA/OGC]Returns the n-th patch
+   */
+  inline Polygon &
+  patchN(size_t const &n)
+  {
+    BOOST_ASSERT(n < _polygons.size());
+    return _polygons[n];
+  }
+  /**
+   * add a patch to the PolyhedralSurface
+   */
+  void
+  addPatch(const Polygon &polygon);
+  /**
+   * add a patch to the PolyhedralSurface
+   */
+  void
+  addPatch(Polygon *polygon);
+  /**
+   * add patchs from an other PolyhedralSurface
+   */
+  void
+  addPatchs(const PolyhedralSurface &polyhedralSurface);
+
+  /**
    * [SFA/OGC]Returns the n-th polygon
-   * @deprecated see geometryN()
+   * @deprecated see patchN()
    */
   inline const Polygon &
   polygonN(size_t const &n) const
   {
-    BOOST_ASSERT(n < _polygons.size());
-    return _polygons[n];
+    return patchN(n);
   }
   /**
    * [SFA/OGC]Returns the n-th polygon
-   * @deprecated see geometryN()
+   * @deprecated see patchN()
    */
   inline Polygon &
   polygonN(size_t const &n)
   {
-    BOOST_ASSERT(n < _polygons.size());
-    return _polygons[n];
+    return patchN(n);
   }
   /**
    * add a polygon to the PolyhedralSurface
+   * @deprecated see addPatch()
    */
   void
   addPolygon(const Polygon &polygon);
   /**
    * add a polygon to the PolyhedralSurface
+   * @deprecated see addPatch()
    */
   void
   addPolygon(Polygon *polygon);
   /**
    * add polygons from an other PolyhedralSurface
+   * @deprecated see addPatchs()
    */
   void
   addPolygons(const PolyhedralSurface &polyhedralSurface);

src/Transform.cpp
@@ -102,8 +102,8 @@ Transform::visit(GeometryCollection &g)
 void
 Transform::visit(PolyhedralSurface &g)
 {
-  for (size_t i = 0; i < g.numPolygons(); i++) {
-    visit(g.polygonN(i));
+  for (size_t i = 0; i < g.numPatchs(); i++) {
+    visit(g.patchN(i));
   }
 }
 

src/algorithm/area.cpp
@@ -141,8 +141,8 @@ area(const PolyhedralSurface &g) -> double
 {
   double result = 0.0;
 
-  for (size_t i = 0; i < g.numPolygons(); i++) {
-    result += area(g.polygonN(i));
+  for (size_t i = 0; i < g.numPatchs(); i++) {
+    result += area(g.patchN(i));
   }
 
   return result;
@@ -273,8 +273,8 @@ area3D(const PolyhedralSurface &g) -> double
 {
   double area = 0.0;
 
-  for (size_t i = 0; i < g.numPolygons(); i++) {
-    area += area3D(g.polygonN(i));
+  for (size_t i = 0; i < g.numPatchs(); i++) {
+    area += area3D(g.patchN(i));
   }
 
   return area;

src/algorithm/buffer3D.cpp
@@ -186,7 +186,7 @@ Buffer3D::computeCylSphereBuffer() const -> std::unique_ptr<PolyhedralSurface>
 
   // Convert the merged mesh to PolyhedralSurface and return
   auto resultSurface = std::make_unique<PolyhedralSurface>();
-  resultSurface->addPolygons(merged_mesh);
+  resultSurface->addPatchs(merged_mesh);
 
   return resultSurface;
 }

src/algorithm/connection.cpp
@@ -107,13 +107,13 @@ SurfaceGraph::addRing(const LineString &ring, FaceIndex faceIndex)
 SurfaceGraph::SurfaceGraph(const PolyhedralSurface &surf)
     : _numVertices(0), _isValid(Validity::valid())
 {
-  const size_t numPolygons = surf.numPolygons();
+  const size_t numPatchs = surf.numPatchs();
 
-  for (size_t p = 0; p != numPolygons; ++p) { // for each polygon
+  for (size_t p = 0; p != numPatchs; ++p) { // for each polygon
     const FaceIndex idx = boost::add_vertex(_graph);
     BOOST_ASSERT(idx == p);
     (void)idx;
-    const Polygon &polygon  = surf.polygonN(p);
+    const Polygon &polygon  = surf.patchN(p);
     const size_t   numRings = polygon.numRings();
 
     for (size_t r = 0; r != numRings; ++r) { // for each ring

commit a1f888f2196ffe4e2695c6b1a7ffad4f087a2fdf

fix(polyhedralsurface): Remove uneeded return
Functions are void

Fixes: edb8addff0bedb343344dfa636c74d7c6c263912

src/PolyhedralSurface.cpp
@@ -212,19 +212,19 @@ PolyhedralSurface::addPatchs(const PolyhedralSurface &polyhedralSurface)
 void
 PolyhedralSurface::addPolygon(const Polygon &polygon)
 {
-  return addPatch(polygon);
+  addPatch(polygon);
 }
 
 void
 PolyhedralSurface::addPolygon(Polygon *polygon)
 {
-  return addPatch(polygon);
+  addPatch(polygon);
 }
 
 void
 PolyhedralSurface::addPolygons(const PolyhedralSurface &polyhedralSurface)
 {
-  return addPatchs(polyhedralSurface);
+  addPatchs(polyhedralSurface);
 }
 
 void