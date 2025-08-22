Oslandia/SFCGAL_CI

commit 62338b5ae5da076717153c9245d5dad355fd59ee

doc: hide internal/private components useless for documentation

src/capi/sfcgal_c.cpp
@@ -77,6 +77,13 @@
 // SFCGAL::PreparedGeometry has no vtable and can thus be manipuled through
 // reinterpret_cast without problem
 
+// ----------------------------------------------------------------------------------
+// -- private interface
+// ----------------------------------------------------------------------------------
+/// @{
+/// @privatesection
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
 static sfcgal_error_handler_t __sfcgal_warning_handler = printf;
 static sfcgal_error_handler_t __sfcgal_error_handler   = printf;
 
@@ -228,6 +235,13 @@ alloc_and_copy(const std::string str, char **buffer, size_t *len) -> void
     *len = 0;
   }
 }
+
+/// @} end of private section
+#endif
+// ----------------------------------------------------------------------------------
+// -- public interface
+// ----------------------------------------------------------------------------------
+/// @publicsection
 
 extern "C" void
 sfcgal_set_error_handlers(sfcgal_error_handler_t warning_handler,
@@ -1035,6 +1049,9 @@ sfcgal_io_read_ewkt(const char *str, size_t len) -> sfcgal_prepared_geometry_t *
   return pg;
 }
 
+/// @{
+/// @privatesection
+
 SFCGAL_GEOMETRY_FUNCTION_BINARY_PREDICATE(covers, SFCGAL::algorithm::covers)
 SFCGAL_GEOMETRY_FUNCTION_BINARY_PREDICATE(covers_3d,
                                           SFCGAL::algorithm::covers3D)
@@ -1073,6 +1090,8 @@ SFCGAL_GEOMETRY_FUNCTION_UNARY_CONSTRUCTION(tesselate,
 
 SFCGAL_GEOMETRY_FUNCTION_UNARY_MEASURE(area, SFCGAL::algorithm::area)
 SFCGAL_GEOMETRY_FUNCTION_UNARY_MEASURE(area_3d, SFCGAL::algorithm::area3D)
+
+/// @} end of private section
 
 extern "C" auto
 sfcgal_geometry_volume(const sfcgal_geometry_t *ga) -> double

src/Geometry.h
@@ -26,6 +26,9 @@ class Object;
 typedef uint32_t srid_t;
 
 namespace SFCGAL {
+
+/// @{
+/// @privatesection
 class Geometry;
 class Point;
 class LineString;
@@ -48,13 +51,11 @@ class Envelope;
 
 class GeometryVisitor;
 class ConstGeometryVisitor;
-} // namespace SFCGAL
-
-namespace SFCGAL {
 
 const uint32_t wkbSRID = 0x20000000;
 const uint32_t wkbM    = 0x40000000;
 const uint32_t wkbZ    = 0x80000000;
+/// @} end of private section
 
 /**
  * [OGC/SFA]8.2.3 "A common list of codes for geometric types"

src/Kernel.h
@@ -19,8 +19,11 @@ namespace SFCGAL {
 
 using Kernel = CGAL::Exact_predicates_exact_constructions_kernel;
 
+/// @{
+/// @privatesection
 /**
  * Quotient type
+ * @private
  */
 typedef CGAL::Gmpq QT;
 
@@ -39,6 +42,8 @@ using Segment_3      = CGAL::Segment_3<Kernel>;
 using Vector_3       = CGAL::Vector_3<SFCGAL::Kernel>;
 using Polyhedron_3   = CGAL::Polyhedron_3<Kernel>;
 using Surface_mesh_3 = CGAL::Surface_mesh<Point_3>;
+
+/// @} end of private section
 
 } // namespace SFCGAL
 

src/Point.cpp
@@ -217,6 +217,8 @@ Point::almostEqual(const Point &other, const double tolerance) const -> bool
   return _coordinate.almostEqual(other._coordinate, tolerance);
 }
 
+/// @{
+/// @privatesection
 ///
 /// Private structures used to implement partial function specialization
 template <int D>
@@ -236,6 +238,8 @@ struct do_toPoint_d<3> {
     return p->toPoint_3();
   }
 };
+
+/// @} end of private section
 
 template <int Dim>
 auto

src/Sphere.cpp
@@ -6,6 +6,12 @@
 
 namespace SFCGAL {
 
+// ----------------------------------------------------------------------------------
+// -- private interface
+// ----------------------------------------------------------------------------------
+/// @{
+/// @privatesection
+
 /**
  * @brief Helper class for building the sphere polyhedron
  */
@@ -135,6 +141,13 @@ private:
   Point_3          center;
   Kernel::Vector_3 direction;
 };
+
+/// @} end of private section
+
+// ----------------------------------------------------------------------------------
+// -- public interface
+// ----------------------------------------------------------------------------------
+/// @publicsection
 
 Sphere::Sphere(const Kernel::FT &radius, const Kernel::Point_3 &center,
                int num_vertical, int num_horizontal,

src/algorithm/Intersection2D.cpp
@@ -18,6 +18,12 @@ using namespace SFCGAL::detail;
 
 namespace SFCGAL::algorithm {
 
+// ----------------------------------------------------------------------------------
+// -- private interface
+// ----------------------------------------------------------------------------------
+/// @{
+/// @privatesection
+
 // local function : get the number of intersection points between rings of a
 // polygon
 auto
@@ -49,6 +55,13 @@ numIntersectionPoints(const CGAL::Polygon_with_holes_2<Kernel> &poly) -> int
 
   return numIntersectionPoints;
 }
+
+/// @} end of private section
+
+// ----------------------------------------------------------------------------------
+// -- public interface
+// ----------------------------------------------------------------------------------
+/// @publicsection
 
 ///
 /// must be called with pa's dimension larger than pb's

src/algorithm/Intersection3D.cpp
@@ -21,6 +21,12 @@ using namespace SFCGAL::detail;
 
 namespace SFCGAL::algorithm {
 
+// ----------------------------------------------------------------------------------
+// -- private interface
+// ----------------------------------------------------------------------------------
+/// @{
+/// @privatesection
+
 void
 _intersection_solid_segment(const PrimitiveHandle<3> &pa,
                             const PrimitiveHandle<3> &pb,
@@ -205,6 +211,13 @@ _intersection_solid_solid(const MarkedPolyhedron &pa,
     }
   }
 }
+
+/// @} end of private section
+
+// ----------------------------------------------------------------------------------
+// -- public interface
+// ----------------------------------------------------------------------------------
+/// @publicsection
 
 ///
 /// must be called with pa's dimension larger than pb's

src/algorithm/alphaShapes.cpp
@@ -28,6 +28,12 @@
 
 namespace SFCGAL::algorithm {
 
+// ----------------------------------------------------------------------------------
+// -- private interface
+// ----------------------------------------------------------------------------------
+/// @{
+/// @privatesection
+
 using Vb              = CGAL::Alpha_shape_vertex_base_2<Kernel>;
 using Fb              = CGAL::Alpha_shape_face_base_2<Kernel>;
 using Tds             = CGAL::Triangulation_data_structure_2<Vb, Fb>;
@@ -122,6 +128,13 @@ alpha_to_geometry(const Alpha_shape_2 &A, bool allow_holes)
 
   return result;
 }
+
+/// @} end of private section
+
+// ----------------------------------------------------------------------------------
+// -- public interface
+// ----------------------------------------------------------------------------------
+/// @publicsection
 
 auto
 optimal_alpha_shapes(const Geometry &g, bool allow_holes, size_t nb_components)

commit 75bd7455c4759539361b3561376f4ed679ea1f90

chore(doc/capi): Fix unbalanced grouping commands in capi
This fixes the following issue:

```
sfcgal_c.cpp warning: end of file with unbalanced grouping commands
```

Fixes: 62338b5ae5da076717153c9245d5dad355fd59ee

src/capi/sfcgal_c.cpp
@@ -241,9 +241,8 @@ alloc_and_copy(const std::string str, char **buffer, size_t *len) -> void
     *len = 0;
   }
 }
-
+#endif // ifndef DOXYGEN_SHOULD_SKIP_THIS
 /// @} end of private section
-#endif
 // ----------------------------------------------------------------------------------
 // -- public interface
 // ----------------------------------------------------------------------------------