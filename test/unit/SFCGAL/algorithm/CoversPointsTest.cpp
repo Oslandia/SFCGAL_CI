049bcabdc97385a7da26074ba7e59a7770b1dbbe
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2025-09-25 16:35:12 +0200

    fix(test): fix using namespace to avoid confusion between SFCGAL::algorithm and SFCGAL::detail::algorithm

--- test/unit/SFCGAL/algorithm/CoversPointsTest.cpp
+++ test/unit/SFCGAL/algorithm/CoversPointsTest.cpp
@@ -24,7 +24,6 @@
 #include "SFCGAL/io/wkt.h"
 
 using namespace SFCGAL;
-using namespace SFCGAL::detail;
 using namespace boost::unit_test;
 
 BOOST_AUTO_TEST_SUITE(SFCGAL_algorithm_CoversPointsTest)
@@ -34,10 +33,10 @@ BOOST_AUTO_TEST_CASE(testPointPointCoversPoints)
   Point const pta(0.0, 1.0, 0.0);
   Point const ptb(0.0, 1.0, 0.0);
   Point const ptc(0.0, 0.0, 0.0);
-  BOOST_CHECK_EQUAL(algorithm::coversPoints(pta, ptb), true);
-  BOOST_CHECK_EQUAL(algorithm::coversPoints(pta, ptc), false);
-  BOOST_CHECK_EQUAL(algorithm::coversPoints3D(pta, ptb), true);
-  BOOST_CHECK_EQUAL(algorithm::coversPoints3D(pta, ptc), false);
+  BOOST_CHECK_EQUAL(detail::algorithm::coversPoints(pta, ptb), true);
+  BOOST_CHECK_EQUAL(detail::algorithm::coversPoints(pta, ptc), false);
+  BOOST_CHECK_EQUAL(detail::algorithm::coversPoints3D(pta, ptb), true);
+  BOOST_CHECK_EQUAL(detail::algorithm::coversPoints3D(pta, ptc), false);
 }
 
 BOOST_AUTO_TEST_CASE(testPolygonPolygonCoversPoints)
@@ -48,8 +47,8 @@ BOOST_AUTO_TEST_CASE(testPolygonPolygonCoversPoints)
     std::unique_ptr<Geometry> const p2 = io::readWkt(
         "POLYGON ((-0.5 -0.5,-0.5 0.5,0.5 0.5,0.5 -0.5,-0.5 -0.5))");
 
-    BOOST_CHECK_EQUAL(algorithm::coversPoints(*p1, *p2), true);
-    BOOST_CHECK_EQUAL(algorithm::coversPoints3D(*p1, *p2), true);
+    BOOST_CHECK_EQUAL(detail::algorithm::coversPoints(*p1, *p2), true);
+    BOOST_CHECK_EQUAL(detail::algorithm::coversPoints3D(*p1, *p2), true);
   }
 
   {
@@ -61,8 +60,8 @@ BOOST_AUTO_TEST_CASE(testPolygonPolygonCoversPoints)
         io::readWkt("POLYGON ((0.2 0.2,0.8 0.2,0.8 0.8,0.2 0.8,0.2 0.2))");
 
     // ST_covers would answer false
-    BOOST_CHECK_EQUAL(algorithm::coversPoints(*p1, *p2), true);
-    BOOST_CHECK_EQUAL(algorithm::coversPoints3D(*p1, *p2), true);
+    BOOST_CHECK_EQUAL(detail::algorithm::coversPoints(*p1, *p2), true);
+    BOOST_CHECK_EQUAL(detail::algorithm::coversPoints3D(*p1, *p2), true);
   }
 }
 
@@ -72,15 +71,15 @@ BOOST_AUTO_TEST_CASE(testCollectionCoversPoints)
     {
         std::unique_ptr<Geometry> p1 = io::readWkt( "GEOMETRYCOLLECTION (LINESTRING (0 0 0,0 0 1),LINESTRING (0 0 0,0 1 0),LINESTRING (0 1 1,0 0 1),LINESTRING (0 1 1,0 1 0))" );
         std::unique_ptr<Geometry> p2 = io::readWkt( "TIN (((0 0.5 0.5,0 0 1,0 0 0,0 0.5 0.5)),((0 0 1,0 0.5 0.5,0 1 1,0 0 1)),((0 0.5 0.5,0 0 0,0 1 0,0 0.5 0.5)),((0 0.5 0.5,0 1 0,0 1 1,0 0.5 0.5)))" );
-        std::cout << "p1 covers p2 ? " << algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
-        std::cout << "p2 covers p1 ? " << algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
+        std::cout << "p1 covers p2 ? " << detail::algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
+        std::cout << "p2 covers p1 ? " << detail::algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
     }
 
     {
         std::unique_ptr<Geometry> p1 = io::readWkt( "GEOMETRYCOLLECTION (TRIANGLE ((1 1,0.5 0.5,0 1,1 1)),TRIANGLE ((1 0,0.5 0.5,1 1,1 0)),TRIANGLE ((0.5 0.5,0 0,0 1,0.5 0.5)))" );
         std::unique_ptr<Geometry> p2 = io::readWkt( "TRIANGLE ((1 0, 0 0,0.5 0.5,1 0))" );
-        std::cout << "p1 covers p2 ? " << algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
-        std::cout << "p2 covers p1 ? " << algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
+        std::cout << "p1 covers p2 ? " << detail::algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
+        std::cout << "p2 covers p1 ? " << detail::algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
     }
 #endif
 }
