b2d529f6dc82b8e0737084f9a27544eb1d66c35b
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/algorithm/length.h pet doxygen and clang-tidy

--- src/algorithm/length.h
+++ src/algorithm/length.h
@@ -14,46 +14,55 @@ class LineString;
 class GeometryCollection;
 } // namespace SFCGAL
 
-namespace SFCGAL {
-namespace algorithm {
+namespace SFCGAL::algorithm {
 
 /**
  * @brief Compute the 2D length for a Geometry (0 for incompatible types)
+ * @param g The geometry to compute length for
+ * @return The 2D length of the geometry
  * @note When applied to NURBSCurve geometries, the 2D length
  *   is internally computed on a LineString obtained via
  *   toLineString() with its default parameters.
  */
-SFCGAL_API double
-length(const Geometry &g);
+SFCGAL_API auto
+length(const Geometry &g) -> double;
 /**
  * @brief Compute the 2D length for a LineString
+ * @param g The linestring to compute length for
+ * @return The 2D length of the linestring
  */
-SFCGAL_API double
-length(const LineString &g);
+SFCGAL_API auto
+length(const LineString &g) -> double;
 /**
  * @brief Compute the 2D length for a GeometryCollection
+ * @param g The geometry collection to compute length for
+ * @return The total 2D length of all geometries in the collection
  */
-SFCGAL_API double
-length(const GeometryCollection &g);
+SFCGAL_API auto
+length(const GeometryCollection &g) -> double;
 
 /**
- * @brief Compute the 2D length for a geometry
- * @return the length of the Geometry, 0 for incompatible types
+ * @brief Compute the 3D length for a geometry
+ * @param g The geometry to compute 3D length for
+ * @return the 3D length of the Geometry, 0 for incompatible types
  */
-SFCGAL_API double
-length3D(const Geometry &g);
+SFCGAL_API auto
+length3D(const Geometry &g) -> double;
 /**
  * @brief Compute the 3D length for a LineString
+ * @param g The linestring to compute 3D length for
+ * @return The 3D length of the linestring
  */
-SFCGAL_API double
-length3D(const LineString &g);
+SFCGAL_API auto
+length3D(const LineString &g) -> double;
 /**
  * @brief Compute the 3D length for a GeometryCollection
+ * @param g The geometry collection to compute 3D length for
+ * @return The total 3D length of all geometries in the collection
  */
-SFCGAL_API double
-length3D(const GeometryCollection &g);
+SFCGAL_API auto
+length3D(const GeometryCollection &g) -> double;
 
-} // namespace algorithm
-} // namespace SFCGAL
+} // namespace SFCGAL::algorithm
 
 #endif
