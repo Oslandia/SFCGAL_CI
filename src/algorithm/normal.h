1dfddce1b88eaaa52199e0d0befa4d6e0f4fa898
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/algorithm/normal.h pet doxygen and clang-tidy

--- src/algorithm/normal.h
+++ src/algorithm/normal.h
@@ -10,41 +10,48 @@
 
 #include "SFCGAL/Polygon.h"
 
-namespace SFCGAL {
-namespace algorithm {
+namespace SFCGAL::algorithm {
 
 /**
  * Returns the 3D normal to 3 consecutive points.
+ * @param point1 the first point
+ * @param point2 the second point
+ * @param point3 the third point
+ * @return the 3D normal vector
  */
 template <typename Kernel>
 CGAL::Vector_3<Kernel>
-normal3D(const CGAL::Point_3<Kernel> &a, const CGAL::Point_3<Kernel> &b,
-         const CGAL::Point_3<Kernel> &c)
+normal3D(const CGAL::Point_3<Kernel> &point1,
+         const CGAL::Point_3<Kernel> &point2,
+         const CGAL::Point_3<Kernel> &point3)
 {
   // bc ^ ba
-  return CGAL::cross_product(c - b, a - b);
+  return CGAL::cross_product(point3 - point2, point1 - point2);
 }
 
 /**
  * Returns the 3D normal to a ring (supposed to be planar and closed).
+ * @param lineString the line string representing the ring
+ * @param exact whether to use exact computation (default: true)
+ * @return the 3D normal vector
  * @warning exact allows to avoid double rounding at the end of the computation
  */
 template <typename Kernel>
 CGAL::Vector_3<Kernel>
-normal3D(const LineString &ls, bool exact = true)
+normal3D(const LineString &lineString, bool exact = true)
 {
   // Newell's formula
   typename Kernel::FT nx, ny, nz;
   nx = ny = nz = 0.0;
 
-  for (size_t i = 0; i < ls.numPoints(); ++i) {
-    const Point        &pi = ls.pointN(i);
-    const Point        &pj = ls.pointN((i + 1) % ls.numPoints());
-    typename Kernel::FT zi = pi.z();
-    typename Kernel::FT zj = pj.z();
-    nx += (pi.y() - pj.y()) * (zi + zj);
-    ny += (zi - zj) * (pi.x() + pj.x());
-    nz += (pi.x() - pj.x()) * (pi.y() + pj.y());
+  for (size_t i = 0; i < lineString.numPoints(); ++i) {
+    const Point &pointI = lineString.pointN(i);
+    const Point &pointJ = lineString.pointN((i + 1) % lineString.numPoints());
+    typename Kernel::FT zi = pointI.z();
+    typename Kernel::FT zj = pointJ.z();
+    nx += (pointI.y() - pointJ.y()) * (zi + zj);
+    ny += (zi - zj) * (pointI.x() + pointJ.x());
+    nz += (pointI.x() - pointJ.x()) * (pointI.y() + pointJ.y());
   }
 
   if (exact) {
@@ -57,6 +64,9 @@ normal3D(const LineString &ls, bool exact = true)
 
 /**
  * Returns the 3D normal to a polygon (supposed to be planar).
+ * @param polygon the polygon to compute the normal for
+ * @param exact whether to use exact computation (default: true)
+ * @return the 3D normal vector
  * @warning exact allows to avoid double rounding at the end of the computation
  */
 template <typename Kernel>
@@ -66,7 +76,6 @@ normal3D(const Polygon &polygon, bool exact = true)
   return normal3D<Kernel>(polygon.exteriorRing(), exact);
 }
 
-} // namespace algorithm
-} // namespace SFCGAL
+} // namespace SFCGAL::algorithm
 
 #endif
