a935100e7c81a8af7b330b9a51bc55364eff67e9
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/algorithm/alphaShapes.h pet doxygen and clang-tidy

--- src/algorithm/alphaShapes.h
+++ src/algorithm/alphaShapes.h
@@ -9,28 +9,34 @@
 
 #include "SFCGAL/Geometry.h"
 
-namespace SFCGAL {
-namespace algorithm {
+namespace SFCGAL::algorithm {
 
 /**
  * Compute the 2D alpha shapes for a geometry
  * https://doc.cgal.org/latest/Alpha_shapes_2/index.html#Chapter_2D_Alpha_Shapes
+ * @param geometry the input geometry
+ * @param alpha the alpha value (default: 1)
+ * @param allow_holes whether to allow holes in the result (default: false)
+ * @return the computed alpha shapes as a unique_ptr<Geometry>
  * @since 1.4.1
  */
 SFCGAL_API auto
-alphaShapes(const Geometry &g, double alpha = 1, bool allow_holes = false)
-    -> std::unique_ptr<Geometry>;
+alphaShapes(const Geometry &geometry, double alpha = 1,
+            bool allow_holes = false) -> std::unique_ptr<Geometry>;
 
 /**
  * Compute the optimal 2D alpha shapes for a geometry
  * https://doc.cgal.org/latest/Alpha_shapes_2/index.html#Chapter_2D_Alpha_Shapes
+ * @param geometry the input geometry
+ * @param allow_holes whether to allow holes in the result (default: false)
+ * @param nb_components the number of components (default: 1)
+ * @return the computed optimal alpha shapes as a unique_ptr<Geometry>
  * @since 1.4.1
  */
 SFCGAL_API auto
-optimal_alpha_shapes(const Geometry &g, bool allow_holes = false,
+optimal_alpha_shapes(const Geometry &geometry, bool allow_holes = false,
                      size_t nb_components = 1) -> std::unique_ptr<Geometry>;
 
-} // namespace algorithm
-} // namespace SFCGAL
+} // namespace SFCGAL::algorithm
 
 #endif
