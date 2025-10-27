149789a750789f1e6fb34b6354746879ae77b348
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:46 +0100

    style: src/numeric.cpp pet doxygen and clang-tidy

--- src/numeric.cpp
+++ src/numeric.cpp
@@ -7,61 +7,64 @@
 
 namespace SFCGAL {
 
+/// @private
 auto
-floor(const CGAL::Gmpq &v) -> CGAL::Gmpz
+floor(const CGAL::Gmpq &value) -> CGAL::Gmpz
 {
-  return v.numerator() / v.denominator();
+  return value.numerator() / value.denominator();
 }
 
+/// @private
 auto
-ceil(const CGAL::Gmpq &v) -> CGAL::Gmpz
+ceil(const CGAL::Gmpq &value) -> CGAL::Gmpz
 {
   CGAL::Gmpz result(0);
-  mpz_cdiv_q(result.mpz(), v.numerator().mpz(), v.denominator().mpz());
+  mpz_cdiv_q(result.mpz(), value.numerator().mpz(), value.denominator().mpz());
   return result;
 }
 
+/// @private
 auto
-round(const CGAL::Gmpq &v) -> CGAL::Gmpz
+round(const CGAL::Gmpq &value) -> CGAL::Gmpz
 {
-  if (v < 0) {
-    // ceil( v - 0.5 ) ;
-    return ceil(v - CGAL::Gmpq(1, 2));
+  if (value < 0) {
+    // ceil( value - 0.5 ) ;
+    return ceil(value - CGAL::Gmpq(1, 2));
   }
-  if (v == 0) {
+  if (value == 0) {
     return 0;
-  } // floor( v + 0.5 ) ;
-  return floor(v + CGAL::Gmpq(1, 2));
+  } // floor( value + 0.5 ) ;
+  return floor(value + CGAL::Gmpq(1, 2));
 }
 
 #ifdef CGAL_USE_GMPXX
 auto
-floor(const mpq_class &v) -> mpz_class
+floor(const mpq_class &value) -> mpz_class
 {
-  return v.get_num() / v.get_den();
+  return value.get_num() / value.get_den();
 }
 
 auto
-ceil(const mpq_class &v) -> mpz_class
+ceil(const mpq_class &value) -> mpz_class
 {
   mpz_class result(0);
-  mpz_cdiv_q(result.get_mpz_t(), v.get_num().get_mpz_t(),
-             v.get_den().get_mpz_t());
+  mpz_cdiv_q(result.get_mpz_t(), value.get_num().get_mpz_t(),
+             value.get_den().get_mpz_t());
   return result;
 }
 
 auto
-round(const mpq_class &v) -> mpz_class
+round(const mpq_class &value) -> mpz_class
 {
-  if (v < 0) {
-    // ceil( v - 0.5 ) ;
-    mpq_class const tmp = v - mpq_class(1, 2);
+  if (value < 0) {
+    // ceil( value - 0.5 ) ;
+    mpq_class const tmp = value - mpq_class(1, 2);
     return ceil(tmp);
   }
-  if (v == 0) {
+  if (value == 0) {
     return 0;
-  } // floor( v + 0.5 ) ;
-  mpq_class const tmp = v + mpq_class(1, 2);
+  } // floor( value + 0.5 ) ;
+  mpq_class const tmp = value + mpq_class(1, 2);
   return floor(tmp);
 }
 #endif
