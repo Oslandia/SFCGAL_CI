dbff491b68a089eb06a2d7c6e66d6b8e237b3e4b
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/algorithm/union.h pet doxygen and clang-tidy

--- src/algorithm/union.h
+++ src/algorithm/union.h
@@ -24,49 +24,69 @@ struct NoValidityCheck;
 
 /**
  * Union on 2D geometries.
- * @pre ga and gb are valid geometries
+ * @param geometry1 first geometry
+ * @param geometry2 second geometry
+ * @return union of the two geometries
+ * @pre geometry1 and geometry2 are valid geometries
  */
 SFCGAL_API std::unique_ptr<Geometry>
-           union_(const Geometry &ga, const Geometry &gb);
+           union_(const Geometry &geometry1, const Geometry &geometry2);
 
 /**
  * Union on 2D geometries. No validity check variant
- * @pre ga and gb are valid geometries
+ * @param geometry1 first geometry
+ * @param geometry2 second geometry
+ * @return union of the two geometries
+ * @pre geometry1 and geometry2 are valid geometries
  * @warning No actual validity check is done.
  */
 SFCGAL_API std::unique_ptr<Geometry>
-           union_(const Geometry &ga, const Geometry &gb, NoValidityCheck);
+union_(const Geometry &geometry1, const Geometry &geometry2, NoValidityCheck);
 
 /**
  * Union on 3D geometries. Assume z = 0 if needed
- * @pre ga and gb are valid geometries
+ * @param geometry1 first geometry
+ * @param geometry2 second geometry
+ * @return union of the two geometries
+ * @pre geometry1 and geometry2 are valid geometries
  */
 SFCGAL_API std::unique_ptr<Geometry>
-           union3D(const Geometry &ga, const Geometry &gb);
+           union3D(const Geometry &geometry1, const Geometry &geometry2);
 
 /**
  * Union on 3D geometries. Assume z = 0 if needed
- * @pre ga and gb are valid geometries
- * @warning@ No actual validity check is done
+ * @param geometry1 first geometry
+ * @param geometry2 second geometry
+ * @return union of the two geometries
+ * @pre geometry1 and geometry2 are valid geometries
+ * @warning No actual validity check is done
  */
 SFCGAL_API std::unique_ptr<Geometry>
-           union3D(const Geometry &ga, const Geometry &gb, NoValidityCheck);
+union3D(const Geometry &geometry1, const Geometry &geometry2, NoValidityCheck);
 
 /**
- * @ingroup detail
+ * @brief Compute union of two GeometrySet objects
+ * @param geometrySet1 First geometry set
+ * @param geometrySet2 Second geometry set
+ * @param result Output geometry set containing the union
  */
 template <int Dim>
 void
-union_(const detail::GeometrySet<Dim> &a, const detail::GeometrySet<Dim> &b,
-       detail::GeometrySet<Dim> &);
+union_(const detail::GeometrySet<Dim> &geometrySet1,
+       const detail::GeometrySet<Dim> &geometrySet2,
+       detail::GeometrySet<Dim>       &result);
 
 /**
- * @ingroup detail
+ * @brief Compute union of two PrimitiveHandle objects
+ * @param primitiveHandle1 First primitive handle
+ * @param primitiveHandle2 Second primitive handle
+ * @param result Output geometry set containing the union
  */
 template <int Dim>
 void
-union_(const detail::PrimitiveHandle<Dim> &a,
-       const detail::PrimitiveHandle<Dim> &b, detail::GeometrySet<Dim> &);
+union_(const detail::PrimitiveHandle<Dim> &primitiveHandle1,
+       const detail::PrimitiveHandle<Dim> &primitiveHandle2,
+       detail::GeometrySet<Dim>           &result);
 
 } // namespace algorithm
 } // namespace SFCGAL
