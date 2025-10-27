8d456445fb7c3a40737f3fd8b49bd8b3296e417b
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/LineString.h pet doxygen and clang-tidy

--- src/LineString.h
+++ src/LineString.h
@@ -25,8 +25,10 @@ namespace SFCGAL {
  */
 class SFCGAL_API LineString : public GeometryImpl<LineString, Geometry> {
 public:
+  /// @brief Iterator type for linestring points
   using iterator =
       DereferenceIterator<std::vector<std::unique_ptr<Point>>::iterator>;
+  /// @brief Const iterator type for linestring points
   using const_iterator =
       DereferenceIterator<std::vector<std::unique_ptr<Point>>::const_iterator>;
 
@@ -36,56 +38,81 @@ public:
   LineString();
   /**
    * Constructor with a point vector
+   * @param points Vector of points to initialize the linestring
    */
   LineString(const std::vector<Point> &points);
   /**
    * LineString constructor
+   * @param startPoint The starting point of the linestring
+   * @param endPoint The ending point of the linestring
    */
   LineString(const Point &startPoint, const Point &endPoint);
   /**
    * Copy constructor
+   * @param other The linestring to copy from
    */
   LineString(LineString const &other);
 
   /**
    * assign operator
+   * @param other The linestring to assign from
+   * @return Reference to this linestring
    */
-  LineString &
-  operator=(LineString other);
+  auto
+  operator=(LineString other) -> LineString &;
 
   /**
    * destructor
    */
-  ~LineString();
+  ~LineString() override;
 
   //-- SFCGAL::Geometry
-  std::string
-  geometryType() const override;
+  /// @brief Get the geometry type as string
+  /// @return "LineString"
+  [[nodiscard]] auto
+  geometryType() const -> std::string override;
   //-- SFCGAL::Geometry
-  GeometryType
-  geometryTypeId() const override;
+  /// @brief Get the geometry type identifier
+  /// @return TYPE_LINESTRING
+  [[nodiscard]] auto
+  geometryTypeId() const -> GeometryType override;
   //-- SFCGAL::Geometry
-  int
-  dimension() const override;
+  /// @brief Get the dimension of the linestring
+  /// @return 1 (linestrings are 1-dimensional)
+  [[nodiscard]] auto
+  dimension() const -> int override;
   //-- SFCGAL::Geometry
-  int
-  coordinateDimension() const override;
+  /// @brief Get the coordinate dimension
+  /// @return Number of coordinates per point
+  [[nodiscard]] auto
+  coordinateDimension() const -> int override;
   //-- SFCGAL::Geometry
-  bool
-  isEmpty() const override;
+  /// @brief Check if the linestring is empty
+  /// @return true if empty, false otherwise
+  [[nodiscard]] auto
+  isEmpty() const -> bool override;
   //-- SFCGAL::Geometry
-  bool
-  is3D() const override;
+  /// @brief Check if the linestring has 3D coordinates
+  /// @return true if 3D, false otherwise
+  [[nodiscard]] auto
+  is3D() const -> bool override;
   //-- SFCGAL::Geometry
-  bool
-  isMeasured() const override;
+  /// @brief Check if the linestring has measured coordinates
+  /// @return true if measured, false otherwise
+  [[nodiscard]] auto
+  isMeasured() const -> bool override;
 
+  /// @brief Drop Z coordinate from all points
+  /// @return true if Z was dropped, false otherwise
   auto
   dropZ() -> bool override;
 
+  /// @brief Drop M coordinate from all points
+  /// @return true if M was dropped, false otherwise
   auto
   dropM() -> bool override;
 
+  /// @brief Swap X and Y coordinates of all points
   auto
   swapXY() -> void override;
 
@@ -103,33 +130,39 @@ public:
 
   /**
    * [SFA/OGC]Returns the number of points
+   * @return Number of points in the linestring
    */
-  inline size_t
-  numPoints() const
+  [[nodiscard]] auto
+  numPoints() const -> size_t
   {
     return _points.size();
   }
   /**
    * Returns the number of segments
+   * @return Number of segments in the linestring
    * @warning not standard, returns zero if LineString contains only one point
    */
-  size_t
-  numSegments() const;
+  [[nodiscard]] auto
+  numSegments() const -> size_t;
 
   /**
    * [SFA/OGC]Returns the n-th point
+   * @param n The index of the point to get
+   * @return Const reference to the nth point
    */
-  inline const Point &
-  pointN(size_t const &n) const
+  [[nodiscard]] auto
+  pointN(size_t const &n) const -> const Point &
   {
     BOOST_ASSERT(n < numPoints());
     return *_points[n];
   }
   /**
    * [SFA/OGC]Returns the n-th point
+   * @param n The index of the point to get
+   * @return Reference to the nth point
    */
-  inline Point &
-  pointN(size_t const &n)
+  auto
+  pointN(size_t const &n) -> Point &
   {
     BOOST_ASSERT(n < numPoints());
     return *_points[n];
@@ -137,34 +170,38 @@ public:
 
   /**
    * [SFA/OGC]Returns the first point
+   * @return Const reference to the first point
    */
-  inline const Point &
-  startPoint() const
+  [[nodiscard]] auto
+  startPoint() const -> const Point &
   {
     return *_points.front();
   }
   /**
    * [SFA/OGC]Returns the first point
+   * @return Reference to the first point
    */
-  inline Point &
-  startPoint()
+  auto
+  startPoint() -> Point &
   {
     return *_points.front();
   }
 
   /**
-   * [SFA/OGC]Returns the first point
+   * @brief [SFA/OGC]Returns the last point
+   * @return Const reference to the last point
    */
-  inline const Point &
-  endPoint() const
+  [[nodiscard]] auto
+  endPoint() const -> const Point &
   {
     return *_points.back();
   }
   /**
-   * [SFA/OGC]Returns the first point
+   * @brief [SFA/OGC]Returns the last point
+   * @return Reference to the last point
    */
-  inline Point &
-  endPoint()
+  auto
+  endPoint() -> Point &
   {
     return *_points.back();
   }
@@ -206,15 +243,16 @@ public:
   //-- methods
 
   /**
-   * test if the LineString is closed
+   * @brief test if the LineString is closed
+   * @return true if linestring is closed, false otherwise
    */
-  bool
-  isClosed() const;
+  [[nodiscard]] auto
+  isClosed() const -> bool;
 
   /**
    * closes the LineString
    */
-  inline void
+  void
   closes()
   {
     _points.push_back(_points.front()->clone());
@@ -222,30 +260,50 @@ public:
 
   //-- iterators
 
-  inline iterator
-  begin()
+  /**
+   * @brief Get iterator to beginning of points
+   * @return Iterator to first point
+   */
+  auto
+  begin() -> iterator
   {
     return dereference_iterator(_points.begin());
   }
-  inline const_iterator
-  begin() const
+  /**
+   * @brief Get const iterator to beginning of points
+   * @return Const iterator to first point
+   */
+  [[nodiscard]] auto
+  begin() const -> const_iterator
   {
     return dereference_iterator(_points.begin());
   }
 
-  inline iterator
-  end()
+  /**
+   * @brief Get iterator to end of points
+   * @return Iterator to past-the-end
+   */
+  auto
+  end() -> iterator
   {
     return dereference_iterator(_points.end());
   }
-  inline const_iterator
-  end() const
+  /**
+   * @brief Get const iterator to end of points
+   * @return Const iterator to past-the-end
+   */
+  [[nodiscard]] auto
+  end() const -> const_iterator
   {
     return dereference_iterator(_points.end());
   }
 
   //-- optimization
 
+  /**
+   * @brief Reserve space for points
+   * @param n Number of points to reserve space for
+   */
   void
   reserve(const size_t &n);
 
@@ -258,7 +316,10 @@ public:
                                       Kernel::Point_2 const,
                                       boost::bidirectional_traversal_tag> {
   public:
-    Point_2_const_iterator() {}
+    /// @brief Default constructor
+    Point_2_const_iterator() = default;
+    /// @brief Constructor from const_iterator
+    /// @param it Iterator to construct from
     explicit Point_2_const_iterator(const_iterator it) : it_(it) {}
     // Point_2_const_iterator( const Point_2_const_iterator<K>& other ) :
     // it_(other.it_) {}
@@ -274,13 +335,13 @@ public:
     {
       it_--;
     }
-    bool
-    equal(const Point_2_const_iterator &other) const
+    auto
+    equal(const Point_2_const_iterator &other) const -> bool
     {
       return this->it_ == other.it_;
     }
-    const Kernel::Point_2 &
-    dereference() const
+    auto
+    dereference() const -> const Kernel::Point_2 &
     {
       p_ = it_->toPoint_2();
       return p_;
@@ -288,18 +349,30 @@ public:
     mutable Kernel::Point_2 p_;
     const_iterator          it_;
   };
-  Point_2_const_iterator
-  points_2_begin() const
+  /**
+   * @brief Get iterator to beginning of 2D points
+   * @return Iterator to first 2D point
+   */
+  [[nodiscard]] auto
+  points_2_begin() const -> Point_2_const_iterator
   {
     return Point_2_const_iterator(begin());
   }
-  Point_2_const_iterator
-  points_2_end() const
+  /**
+   * @brief Get iterator to end of 2D points
+   * @return Iterator to past-the-end of 2D points
+   */
+  [[nodiscard]] auto
+  points_2_end() const -> Point_2_const_iterator
   {
     return Point_2_const_iterator(end());
   }
-  std::pair<Point_2_const_iterator, Point_2_const_iterator>
-  points_2() const
+  /**
+   * @brief Get pair of iterators for 2D points
+   * @return Pair of begin and end iterators for 2D points
+   */
+  [[nodiscard]] auto
+  points_2() const -> std::pair<Point_2_const_iterator, Point_2_const_iterator>
   {
     return std::make_pair(points_2_begin(), points_2_end());
   }
@@ -313,8 +386,13 @@ public:
                                       Kernel::Point_3 const,
                                       boost::bidirectional_traversal_tag> {
   public:
-    Point_3_const_iterator() {}
+    /// @brief Default constructor
+    Point_3_const_iterator() = default;
+    /// @brief Constructor from const_iterator
+    /// @param it Iterator to construct from
     explicit Point_3_const_iterator(const_iterator it) : it_(it) {}
+    /// @brief Copy constructor
+    /// @param other Point_3_const_iterator to copy from
     Point_3_const_iterator(const Point_3_const_iterator &other) : it_(other.it_)
     {
     }
@@ -331,13 +409,13 @@ public:
     {
       it_--;
     }
-    bool
-    equal(const Point_3_const_iterator &other) const
+    auto
+    equal(const Point_3_const_iterator &other) const -> bool
     {
       return this->it_ == other.it_;
     }
-    const Kernel::Point_3 &
-    dereference() const
+    auto
+    dereference() const -> const Kernel::Point_3 &
     {
       p_ = it_->toPoint_3();
       return p_;
@@ -346,41 +424,58 @@ public:
     const_iterator          it_;
   };
 
-  Point_3_const_iterator
-  points_3_begin() const
+  /**
+   * @brief Get iterator to beginning of 3D points
+   * @return Iterator to first 3D point
+   */
+  [[nodiscard]] auto
+  points_3_begin() const -> Point_3_const_iterator
   {
     return Point_3_const_iterator(begin());
   }
-  Point_3_const_iterator
-  points_3_end() const
+  /**
+   * @brief Get iterator to end of 3D points
+   * @return Iterator to past-the-end of 3D points
+   */
+  [[nodiscard]] auto
+  points_3_end() const -> Point_3_const_iterator
   {
     return Point_3_const_iterator(end());
   }
-  std::pair<Point_3_const_iterator, Point_3_const_iterator>
-  points_3() const
+  /**
+   * @brief Get pair of iterators for 3D points
+   * @return Pair of begin and end iterators for 3D points
+   */
+  [[nodiscard]] auto
+  points_3() const -> std::pair<Point_3_const_iterator, Point_3_const_iterator>
   {
     return std::make_pair(points_3_begin(), points_3_end());
   }
 
-  /*
+  /**
    * @brief Convert to CGAL::Polygon_2
-   * @param forceCounterClocksize force exterior ring orientation (counter
-   * clocksize)
+   * @param fixOrientation Force exterior ring orientation (counter clockwise)
+   * @return CGAL 2D polygon representation
    */
-  CGAL::Polygon_2<Kernel>
-  toPolygon_2(bool fixOrientation = true) const;
+  [[nodiscard]] auto
+  toPolygon_2(bool fixOrientation = true) const -> CGAL::Polygon_2<Kernel>;
 
   //-- visitors
 
   //-- SFCGAL::Geometry
+  /// @brief Accept a geometry visitor
+  /// @param visitor Visitor to accept
   void
   accept(GeometryVisitor &visitor) override;
   //-- SFCGAL::Geometry
+  /// @brief Accept a const geometry visitor
+  /// @param visitor Const visitor to accept
   void
   accept(ConstGeometryVisitor &visitor) const override;
 
   /**
-   * Serializer
+   * @brief Serializer
+   * @param ar Archive for serialization
    */
   template <class Archive>
   void
@@ -394,7 +489,7 @@ private:
   std::vector<std::unique_ptr<Point>> _points;
 
   void
-  swap(LineString &other)
+  swap(LineString &other) noexcept
   {
     std::swap(_points, other._points);
   }
