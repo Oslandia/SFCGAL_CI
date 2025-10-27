6b273624b253a735b39ea368e6417c0e375a9eaf
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:45 +0100

    style: src/PreparedGeometry.h pet doxygen and clang-tidy

--- src/PreparedGeometry.h
+++ src/PreparedGeometry.h
@@ -39,54 +39,65 @@ public:
   /**
    * Constructor
    * @param geometry pointer to the underlying SFCGAL::Geometry. Takes ownership
+   * @param srid The spatial reference system identifier
    */
   PreparedGeometry(std::unique_ptr<Geometry> &&geometry, srid_t srid = 0);
 
   /**
    * Constructor
    * @param geometry pointer to the underlying SFCGAL::Geometry. Takes ownership
+   * @param srid The spatial reference system identifier
    */
   PreparedGeometry(Geometry *geometry, srid_t srid = 0);
 
-  virtual ~PreparedGeometry();
+  ~PreparedGeometry();
 
   /**
    * Geometry accessors
+   * @return Const reference to the geometry
    */
-  const Geometry &
-  geometry() const;
-  Geometry &
-  geometry();
+  auto
+  geometry() const -> const Geometry &;
+  /**
+   * Geometry accessor
+   * @return Reference to the geometry
+   */
+  auto
+  geometry() -> Geometry &;
 
   /**
    * Geometry setter
+   * @param geom The new geometry to set
    */
   void
   resetGeometry(Geometry *geom);
 
   /**
    * SRID read only accessor
+   * @return The spatial reference system identifier
    */
-  const srid_t &
-  SRID() const
+  [[nodiscard]] auto
+  SRID() const -> const srid_t &
   {
     return _srid;
   }
 
   /**
    * SRID accessor
+   * @return Reference to the spatial reference system identifier
    */
-  srid_t &
-  SRID()
+  auto
+  SRID() -> srid_t &
   {
     return _srid;
   }
 
   /**
    * Envelope accessor (using cache)
+   * @return The envelope of the geometry
    */
-  const Envelope &
-  envelope() const;
+  [[nodiscard]] auto
+  envelope() const -> const Envelope &;
 
   /**
    * Resets the cache
@@ -95,19 +106,27 @@ public:
   invalidateCache();
 
   /**
-   * Convert to an extended WKT (with SRID)
-   * @param numDecimals: number of decimals, -1 for keeping the exact rational
+   * @brief Convert to an extended WKT (with SRID)
+   * @param numDecimals number of decimals, -1 for keeping the exact rational
    * representation, if possible
+   * @return Extended WKT representation with SRID
    */
-  std::string
-  asEWKT(const int &numDecimals = -1) const;
+  [[nodiscard]] auto
+  asEWKT(const int &numDecimals = -1) const -> std::string;
 
+  /**
+   * @brief Convert to Extended Well-Known Binary
+   * @param wkbOrder Byte order for WKB output
+   * @param asHex Return as hexadecimal string if true
+   * @return Extended WKB representation
+   */
   auto
   asEWKB(boost::endian::order wkbOrder = boost::endian::order::native,
          bool                 asHex    = false) const -> std::string;
 
   /**
-   * Serializer
+   * @brief Save prepared geometry to archive
+   * @param ar Archive to save to
    */
   template <class Archive>
   void
@@ -118,6 +137,10 @@ public:
     ar & pgeom;
   }
 
+  /**
+   * @brief Load prepared geometry from archive
+   * @param ar Archive to load from
+   */
   template <class Archive>
   void
   load(Archive &ar, const unsigned int /*version*/)
@@ -128,6 +151,11 @@ public:
     _geometry.reset(pgeom);
   }
 
+  /**
+   * @brief Serialize prepared geometry to/from archive
+   * @param ar Archive for serialization
+   * @param version Serialization version
+   */
   template <class Archive>
   void
   serialize(Archive &ar, const unsigned int version)
@@ -136,13 +164,13 @@ public:
   }
 
 protected:
-  // Pointer to underlying Geometry
+  /// @brief Pointer to underlying Geometry
   std::unique_ptr<Geometry> _geometry;
 
-  // SRID of the geometry
+  /// @brief SRID of the geometry
   srid_t _srid;
 
-  // bbox of the geometry
+  /// @brief Cached bounding box of the geometry
   mutable boost::optional<Envelope> _envelope;
 };
 
