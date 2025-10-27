8196c463afc00d7f23fef179e32cb166168a5eaf
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-27 09:10:46 +0100

    style: src/io/wkb.h pet doxygen and clang-tidy

--- src/io/wkb.h
+++ src/io/wkb.h
@@ -17,54 +17,72 @@ class Geometry;
 class PreparedGeometry;
 } // namespace SFCGAL
 
-namespace SFCGAL {
-namespace io {
+namespace SFCGAL::io {
 
 // WKB
 
 /**
- * Read a WKB geometry from an input stream
+ * @brief Read a WKB geometry from an input stream
+ * @param stream The input stream to read from
+ * @param asHexString Whether the WKB data is encoded as hex string
+ * @return A unique pointer to the geometry
  */
 SFCGAL_API auto
 readWkb(std::istream &stream, bool asHexString = false)
     -> std::unique_ptr<Geometry>;
 /**
- * Read a WKB geometry from a string
+ * @brief Read a WKB geometry from a string
+ * @param s The string containing WKB data
+ * @param asHexString Whether the WKB data is encoded as hex string
+ * @return A unique pointer to the geometry
  */
 SFCGAL_API auto
 readWkb(const std::string &s, bool asHexString = false)
     -> std::unique_ptr<Geometry>;
 
 /**
- * Read a WKB geometry from a char*
+ * @brief Read a WKB geometry from a char array
+ * @param str The character array containing WKB data
+ * @param len The length of the character array
+ * @param asHexString Whether the WKB data is encoded as hex string
+ * @return A unique pointer to the geometry
  */
 SFCGAL_API auto
-readWkb(const char *, size_t, bool asHexString = false)
+readWkb(const char *str, size_t len, bool asHexString = false)
     -> std::unique_ptr<Geometry>;
 
 // EWKB
 
 /**
- * Read a EWKB geometry from an input stream
+ * @brief Read a EWKB geometry from an input stream
+ * @param stream The input stream to read from
+ * @param asHexString Whether the EWKB data is encoded as hex string
+ * @return A unique pointer to the prepared geometry
  */
 SFCGAL_API auto
 readEwkb(std::istream &stream, bool asHexString = false)
     -> std::unique_ptr<PreparedGeometry>;
 /**
- * Read a EWKB geometry from a string
+ * @brief Read a EWKB geometry from a string
+ * @param s The string containing EWKB data
+ * @param asHexString Whether the EWKB data is encoded as hex string
+ * @return A unique pointer to the prepared geometry
  */
 SFCGAL_API auto
 readEwkb(const std::string &s, bool asHexString = false)
     -> std::unique_ptr<PreparedGeometry>;
 
 /**
- * Read a EWKB geometry from a char*
+ * @brief Read a EWKB geometry from a char array
+ * @param str The character array containing EWKB data
+ * @param len The length of the character array
+ * @param asHexString Whether the EWKB data is encoded as hex string
+ * @return A unique pointer to the prepared geometry
  */
 SFCGAL_API auto
-readEwkb(const char *, size_t, bool asHexString = false)
+readEwkb(const char *str, size_t len, bool asHexString = false)
     -> std::unique_ptr<PreparedGeometry>;
 
-} // namespace io
-} // namespace SFCGAL
+} // namespace SFCGAL::io
 
 #endif
