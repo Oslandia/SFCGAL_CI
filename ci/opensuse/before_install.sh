45d5118e0b1f8c9886115cbff4cef51470b71ae6
Jean Felder <jean.felder@oslandia.com>
2025-10-29 09:42:51 +0100

    ci: Remove unused boost-timer dependency

--- ci/opensuse/before_install.sh
+++ ci/opensuse/before_install.sh
@@ -2,7 +2,6 @@ zypper update -y
 zypper install -y \
     cmake cgal-devel ninja gcc gcc-c++ \
     libboost_serialization-devel \
-    libboost_timer-devel \
     libboost_program_options-devel \
     libboost_test-devel \
     nlohmann_json-devel
