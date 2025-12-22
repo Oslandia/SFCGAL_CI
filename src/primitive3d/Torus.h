af6dbc0c378171a7d563ba869d9cde007c331986
Jean Felder <jean.felder@oslandia.com>
2025-12-22 21:58:47 +0100

    chore(torus): Add missing override documentation

--- src/primitive3d/Torus.h
+++ src/primitive3d/Torus.h
@@ -42,9 +42,11 @@ public:
    */
   Torus(const Torus &other) = default;
 
+  /// @copydoc SFCGAL::Primitive::primitiveType
   [[nodiscard]] auto
   primitiveType() const -> std::string override;
 
+  /// @copydoc SFCGAL::Primitive::primitiveTypeId
   [[nodiscard]] auto
   primitiveTypeId() const -> PrimitiveType override;
 
