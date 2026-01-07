16eacc94f4eb9966d905bc2e48e2a3fe4b4c1ec1
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2026-01-07 13:40:51 +0100

    fix(sfcgalop): convert segment to subdivision after sphere change

--- sfcgalop/constructors.cpp
+++ sfcgalop/constructors.cpp
@@ -21,14 +21,12 @@ namespace Constructors {
 
 auto
 make_sphere(double x, double y, double z, double radius,
-            unsigned int num_vertical, unsigned int num_horizontal)
-    -> std::unique_ptr<SFCGAL::Geometry>
+            unsigned int num_subdivisions) -> std::unique_ptr<SFCGAL::Geometry>
 {
   SFCGAL::Kernel::Point_3  center(x, y, z);
   SFCGAL::Kernel::Vector_3 direction(0, 0, 1); // Default up direction
 
-  SFCGAL::Sphere sphere(radius, center, num_vertical, num_horizontal,
-                        direction);
+  SFCGAL::Sphere sphere(radius, center, num_subdivisions, direction);
 
   auto polyhedral_surface = sphere.generatePolyhedralSurface();
   return std::make_unique<SFCGAL::PolyhedralSurface>(
